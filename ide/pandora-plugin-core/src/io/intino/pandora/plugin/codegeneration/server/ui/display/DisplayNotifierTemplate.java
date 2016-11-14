0e79f654c9dcb02d3f3adf70a50f402610b86938
Octavio Roncal <octavioroncal@monentia.es>
2016-11-14 11:18:13 +0000

    UI server code generation

--- ide/pandora-plugin-core/src/io/intino/pandora/plugin/codegeneration/server/ui/display/DisplayNotifierTemplate.java
+++ ide/pandora-plugin-core/src/io/intino/pandora/plugin/codegeneration/server/ui/display/DisplayNotifierTemplate.java
@@ -19,8 +19,11 @@ public class DisplayNotifierTemplate extends Template {
 	public Template define() {
 		add(
 			rule().add((condition("type", "display"))).add(literal("package ")).add(mark("package")).add(literal(".displays.notifiers;\n\nimport io.intino.pandora.exceptions.*;\nimport ")).add(mark("package")).add(literal(".*;\n")).add(mark("schemaImport")).add(literal("\n\npublic class ")).add(mark("name", "FirstUpperCase")).add(literal("DisplayNotifier extends io.intino.pandora.server.activity.displays.DisplayNotifier {\n\n    public ")).add(mark("name", "FirstUpperCase")).add(literal("DisplayNotifier(io.intino.pandora.server.activity.displays.Display display, io.intino.pandora.server.activity.displays.MessageCarrier carrier) {\n        super(display, carrier);\n    }\n\n\t")).add(mark("notification").multiple("\n\n")).add(literal("\n}\n")),
-			rule().add((condition("type", "notification")), (condition("trigger", "notification"))).add(literal("public void ")).add(mark("name", "firstLowercase")).add(literal("(")).add(mark("parameter", "firstUpperCase")).add(literal(" ")).add(mark("parameter", "firstLowerCase")).add(literal(") {\n\tputToOwner(\"")).add(mark("name", "firstLowercase")).add(literal("\"")).add(expression().add(literal(", \"")).add(mark("parameter", "firstLowercase")).add(literal("\", ")).add(mark("parameter", "firstLowercase"))).add(literal(");\n}")),
-			rule().add((condition("type", "schemaImport"))).add(literal("import ")).add(mark("package")).add(literal(".schemas.*;"))
+			rule().add((condition("type", "notification")), (condition("trigger", "notification"))).add(literal("public void ")).add(mark("name", "firstLowercase")).add(literal("(")).add(mark("parameter", "firstUpperCase")).add(literal(" ")).add(mark("parameter", "firstLowerCase")).add(literal(") {\n\tput")).add(expression().add(mark("target"))).add(literal("(\"")).add(mark("name", "firstLowercase")).add(literal("\"")).add(expression().add(literal(", \"")).add(mark("parameter", "firstLowercase")).add(literal("\", ")).add(mark("parameter", "firstLowercase"))).add(literal(");\n}")),
+			rule().add((condition("type", "schemaImport"))).add(literal("import ")).add(mark("package")).add(literal(".schemas.*;")),
+			rule().add((condition("attribute", "All")), (condition("trigger", "target"))).add(literal("ToAll")),
+			rule().add((condition("attribute", "Display")), (condition("trigger", "target"))).add(literal("ToDisplay")),
+			rule().add((condition("attribute", "Client")), (condition("trigger", "target")))
 		);
 		return this;
 	}
