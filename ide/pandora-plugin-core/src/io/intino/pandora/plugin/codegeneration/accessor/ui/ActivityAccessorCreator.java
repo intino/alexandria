d2e02ddd029c9de76131b08dd3841abf8c201c03
Octavio Roncal <octavioroncal@monentia.es>
2016-11-24 12:04:37 +0000

    removed activity accessor Fixed errors in ui templates.

--- ide/pandora-plugin-core/src/io/intino/pandora/plugin/codegeneration/accessor/ui/ActivityAccessorCreator.java
+++ ide/pandora-plugin-core/src/io/intino/pandora/plugin/codegeneration/accessor/ui/ActivityAccessorCreator.java
@@ -1,4 +1,4 @@
-package io.intino.pandora.plugin.actions;
+package io.intino.pandora.plugin.codegeneration.accessor.ui;
 
 import com.intellij.ide.highlighter.ModuleFileType;
 import com.intellij.openapi.application.ApplicationManager;
@@ -10,18 +10,17 @@ import com.intellij.openapi.roots.ModifiableRootModel;
 import com.intellij.openapi.roots.ModuleRootManager;
 import com.intellij.openapi.util.Computable;
 import io.intino.pandora.plugin.Activity;
-import io.intino.pandora.plugin.codegeneration.accessor.ui.ActivityAccessorRenderer;
 import tara.magritte.Graph;
 
 import java.io.File;
 
-class ActivityAccessorCreator {
+public class ActivityAccessorCreator {
 
 	private final Project project;
 	private final Module appModule;
 	private final Activity activity;
 
-	ActivityAccessorCreator(Module module, Graph graph) {
+	public ActivityAccessorCreator(Module module, Graph graph) {
 		this.project = module.getProject();
 		appModule = module;
 		this.activity = graph.find(Activity.class).get(0);
--- ide/pandora-plugin-core/src/io/intino/pandora/plugin/codegeneration/accessor/ui/WidgetNotifierTemplate.java
+++ ide/pandora-plugin-core/src/io/intino/pandora/plugin/codegeneration/accessor/ui/WidgetNotifierTemplate.java
@@ -20,7 +20,7 @@ public class WidgetNotifierTemplate extends Template {
 	public Template define() {
 		add(
 				rule().add((condition("type", "widget"))).add(literal("var ApplicationWidgetBehaviors = ApplicationWidgetBehaviors || {};\n\nApplicationWidgetBehaviors.NotifierListener = {\n\n    listenToDisplay : function() {\n        var widget = this;\n        ")).add(mark("notification").multiple("\n")).add(literal("\n    }\n\n};")),
-				rule().add((condition("trigger", "notification"))).add(literal("this.when(\"")).add(mark("name")).add(literal("\").")).add(expression().add(mark("type"))).add(literal(".execute(function(parameters) {\n\twidget._")).add(mark("name")).add(literal("(")).add(expression().add(mark("parameter"))).add(literal(");\n});")),
+				rule().add((condition("trigger", "notification"))).add(literal("this.when(\"")).add(mark("name")).add(literal("\")")).add(expression().add(mark("type"))).add(literal(".execute(function(parameters) {\n\twidget._")).add(mark("name")).add(literal("(")).add(expression().add(mark("parameter"))).add(literal(");\n});")),
 			rule().add((condition("trigger", "parameter"))).add(literal("parameters.value")),
 			rule().add((condition("attribute", "Display")), (condition("trigger", "type"))).add(literal(".toSelf()")),
 			rule().add((condition("trigger", "type")))
