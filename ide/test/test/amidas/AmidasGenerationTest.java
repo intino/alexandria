2ca7343a387ed8b73372c209cd5829367217b4ec
Octavio Roncal <octavioroncal@monentia.es>
2017-02-10 10:52:06 +0000

    updated resources in rest

--- ide/test/test/amidas/AmidasGenerationTest.java
+++ ide/test/test/amidas/AmidasGenerationTest.java
@@ -1,7 +1,7 @@
 package amidas;
 
-import io.intino.konos.model.Konos;
 import io.intino.konos.builder.codegeneration.FullRenderer;
+import io.intino.konos.model.Konos;
 import io.intino.tara.magritte.Graph;
 import org.junit.Test;
 
@@ -17,8 +17,6 @@ public class AmidasGenerationTest {
 		File gen = new File("test-gen", AMIDAS);
 		final Graph graph = Graph.use(Konos.class, null).load("Amidas");
 		new FullRenderer(null, graph, gen, gen, AMIDAS).execute();
-//		for (JMSService jmsService : graph.find(JMSService.class))
-//			new JMSAccessorRenderer(jmsService).execute(gen, AMIDAS);
 	}
 
 	@Test
