bd7a68e7b220d77fe122295453cf19bd23948f70
Octavio Roncal <octavioroncal@monentia.es>
2017-02-16 10:00:52 +0000

    added adapters to instant and date

--- server/src/io/intino/konos/server/spark/SparkReader.java
+++ server/src/io/intino/konos/server/spark/SparkReader.java
@@ -1,6 +1,7 @@
 package io.intino.konos.server.spark;
 
-import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializer;
 import io.intino.konos.Error;
 
 import java.io.InputStream;
@@ -12,6 +13,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Collection;
+import java.util.Date;
 
 import static java.time.Instant.ofEpochMilli;
 import static java.time.LocalDateTime.ofInstant;
@@ -20,7 +22,8 @@ import static java.time.LocalDateTime.ofInstant;
 class SparkReader {
 
 	static <T> T read(String object, Class<T> type) {
-		if (type.isAssignableFrom(Error.class) || type.isAssignableFrom(Collection.class)) return adaptFromJSON(object, type);
+		if (type.isAssignableFrom(Error.class) || type.isAssignableFrom(Collection.class))
+			return adaptFromJSON(object, type);
 		else if (type.isAssignableFrom(byte[].class)) return (T) readBytes(object);
 		return adapt(object, type);
 	}
@@ -40,7 +43,10 @@ class SparkReader {
 
 	private static <T> T adaptFromJSON(String object, Class<T> type) {
 		try {
-			return object == null || object.isEmpty() ? null : new Gson().fromJson(URLDecoder.decode(object, "UTF-8"), type);
+			final GsonBuilder builder = new GsonBuilder();
+			builder.registerTypeAdapter(Instant.class, (JsonDeserializer<Instant>) (json, type1, jsonDeserializationContext) -> Instant.ofEpochMilli(json.getAsJsonPrimitive().getAsLong())).
+					registerTypeAdapter(Date.class, (JsonDeserializer<Date>) (json, type1, jsonDeserializationContext) -> new Date(json.getAsJsonPrimitive().getAsLong()));
+			return object == null || object.isEmpty() ? null : builder.create().fromJson(URLDecoder.decode(object, "UTF-8"), type);
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 			return null;
