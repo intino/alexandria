dde1956b62ef1ff36d4eabc9736659b380147113
Octavio Roncal <octavioroncal@monentia.es>
2017-05-23 11:44:33 +0100

    updating box templates.

--- ide/builder/src/io/intino/konos/builder/codegeneration/BoxTemplate.java
+++ ide/builder/src/io/intino/konos/builder/codegeneration/BoxTemplate.java
@@ -18,10 +18,8 @@ public class BoxTemplate extends Template {
 
 	public Template define() {
 		add(
-			rule().add((condition("type", "box"))).add(literal("package ")).add(mark("package")).add(literal(";\n\n")).add(expression().add(mark("tara", "empty")).add(literal("import io.intino.tara.magritte.Graph;"))).add(literal("\n\nclass Box extends AbstractBox {\n\n\tBox(")).add(mark("name", "SnakeCaseToCamelCase", "firstUpperCase")).add(literal("Configuration configuration) {\n\t\tsuper(configuration);\n\t}")).add(expression().add(literal("\n")).add(literal("\n")).add(literal("\t")).add(mark("tara"))).add(expression().add(literal("\n")).add(literal("\n")).add(literal("\t")).add(mark("rest")).add(literal("\n")).add(literal("\t"))).add(literal("\n\tvoid init() {\n\t\tsuper.init();\n\t}\n\n\tvoid start() {\n\t\tsuper.start();\n    }\n\n\tvoid terminate() {\n\t\tsuper.terminate();\n\t}\n\n\n}")),
+			rule().add((condition("type", "box"))).add(literal("package ")).add(mark("package")).add(literal(";\n\nclass Box extends AbstractBox {\n\n\tBox(")).add(mark("name", "SnakeCaseToCamelCase", "firstUpperCase")).add(literal("Configuration configuration) {\n\t\tsuper(configuration);\n\t}")).add(expression().add(literal("\n")).add(literal("\n")).add(literal("\t")).add(mark("rest")).add(literal("\n")).add(literal("\t"))).add(literal("\n\tvoid open() {\n\t\tsuper.open();\n\t}\n\n\tvoid close() {\n\t\tsuper.close();\n\t}\n\n\n}")),
 			rule().add((condition("trigger", "empty"))),
-			rule().add((condition("trigger", "tara"))).add(literal("Graph initGraph() {\n\treturn Graph.use(store(), ")).add(mark("wrapper").multiple(", ")).add(literal(").load(")).add(mark("outDSL", "quoted")).add(literal(");\n}\n\nprivate io.intino.tara.magritte.Store store() {\n\t// return new io.intino.tara.magritte.stores.FileSystemStore(configuration.store());\n\treturn new io.intino.tara.magritte.stores.ResourcesStore();\n}")),
-			rule().add((condition("trigger", "wrapper"))).add(mark("value")).add(literal(".class")),
 			rule().add((condition("trigger", "quoted"))).add(literal("\"")).add(mark("value")).add(literal("\"")),
 			rule().add((condition("trigger", "rest"))).add(literal("static io.intino.konos.server.activity.services.AuthService authService(java.net.URL authServiceUrl) {\n\t//TODO add your authService\n\treturn null;\n}\t"))
 		);
