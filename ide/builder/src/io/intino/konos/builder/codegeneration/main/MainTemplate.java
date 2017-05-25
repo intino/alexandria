278598741565642ee2b9c7e05f0c3990773e47a1
Octavio Roncal <octavioroncal@monentia.es>
2017-05-25 10:52:24 +0100

    updated libraries. and code generation

--- ide/builder/src/io/intino/konos/builder/codegeneration/main/MainTemplate.java
+++ ide/builder/src/io/intino/konos/builder/codegeneration/main/MainTemplate.java
@@ -18,7 +18,7 @@ public class MainTemplate extends Template {
 
 	public Template define() {
 		add(
-			rule().add((condition("type", "main"))).add(literal("package ")).add(mark("package")).add(literal(";\n\npublic class Main {\n\tpublic static void main(String[] args) {\n\t\t")).add(mark("name", "SnakeCaseToCamelCase", "firstUpperCase")).add(literal("Box box = new ")).add(mark("name", "SnakeCaseToCamelCase", "firstUpperCase")).add(literal("Box(args).open();\n\t\tRuntime.getRuntime().addShutdownHook(new Thread(box::close));\n\t}\n}"))
+			rule().add((condition("type", "main"))).add(literal("package ")).add(mark("package")).add(literal(";\n\nimport io.intino.konos.Box;\n\npublic class Main {\n\tpublic static void main(String[] args) {\n\t\tBox box = new ")).add(mark("name", "SnakeCaseToCamelCase", "firstUpperCase")).add(literal("Box(args).open();\n\t\tRuntime.getRuntime().addShutdownHook(new Thread(box::close));\n\t}\n}"))
 		);
 		return this;
 	}
