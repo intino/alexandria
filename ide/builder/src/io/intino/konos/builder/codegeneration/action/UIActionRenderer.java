c0625bd5a9aaaaa6baa41653e9bb34e1410ca7d9
Octavio Roncal <octavioroncal@monentia.es>
2017-06-05 08:31:20 +0100

    updated ui action rendering

--- ide/builder/src/io/intino/konos/builder/codegeneration/action/UIActionRenderer.java
+++ ide/builder/src/io/intino/konos/builder/codegeneration/action/UIActionRenderer.java
@@ -1,9 +1,10 @@
 package io.intino.konos.builder.codegeneration.action;
 
 import com.intellij.openapi.project.Project;
-import io.intino.konos.model.Activity;
 import io.intino.konos.builder.helpers.Commons;
-import org.siani.itrules.model.AbstractFrame;
+import io.intino.konos.model.Activity;
+import io.intino.konos.model.Dialog;
+import io.intino.konos.model.Display;
 import org.siani.itrules.model.Frame;
 
 import java.io.File;
@@ -18,23 +19,16 @@ public class UIActionRenderer extends ActionRenderer {
 	}
 
 	public void execute() {
-		Frame frame = new Frame().addTypes("action");
+		Frame frame = new Frame().addTypes("action", "page");
 		frame.addSlot("name", page.name());
 		frame.addSlot("package", packageName);
 		frame.addSlot("box", boxName);
-		frame.addSlot("returnType", "String");
-		frame.addSlot("parameter", (AbstractFrame[]) parameters());
-		frame.addSlot("ui", "");
+		if (page.uses().is(Dialog.class)) frame.addSlot("importDialogs", packageName);
+		else frame.addSlot("importDisplays", packageName);
+		frame.addSlot("ui", page.uses().name() + (page.uses().is(Dialog.class) ? Dialog.class.getSimpleName() : Display.class.getSimpleName()));
 		if (!alreadyRendered(destiny, page.name()))
 			Commons.writeFrame(destinyPackage(destiny), page.name() + "Action", template().format(frame));
 		//TODO else Update
-	}
-
-	private Frame[] parameters() {
-		final Frame[] frames = new Frame[2];
-		frames[0]= new Frame().addTypes("parameter").addSlot("type", "io.intino.konos.server.activity.services.push.ActivitySession").addSlot("name", "session");
-		frames[1]= new Frame().addTypes("parameter").addSlot("type", "String").addSlot("name", "clientId");
-		return frames;
 	}
 
 }
