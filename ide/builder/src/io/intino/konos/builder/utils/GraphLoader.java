856f8c93cf66f4cc604008b994717f9651b42332
Octavio Roncal <octavioroncal@monentia.es>
2017-06-08 15:06:42 +0100

    updated code generation adding package parameter filling feature.wip

--- ide/builder/src/io/intino/konos/builder/utils/GraphLoader.java
+++ ide/builder/src/io/intino/konos/builder/utils/GraphLoader.java
@@ -1,9 +1,17 @@
 package io.intino.konos.builder.utils;
 
 import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.module.Module;
+import com.intellij.psi.PsiFile;
 import io.intino.konos.model.Konos;
+import io.intino.tara.StashBuilder;
 import io.intino.tara.io.Stash;
 import io.intino.tara.magritte.Graph;
+
+import java.io.File;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 public class GraphLoader {
 	private static final Logger LOG = Logger.getInstance("GraphLoader");
@@ -14,6 +22,17 @@ public class GraphLoader {
 		final Graph graph = Graph.use(Konos.class).load("Konos").loadStashes(stashes);
 		Thread.currentThread().setContextClassLoader(currentLoader);
 		return graph.wrapper(Konos.class);
+	}
+
+	public static Graph loadGraph(Module module) {
+		final List<PsiFile> konosFiles = KonosUtils.findKonosFiles(module);
+		if (!konosFiles.isEmpty()) {
+			final Stash stash = new StashBuilder(konosFiles.stream().map(pf ->
+					new File(pf.getVirtualFile().getPath())).collect(toList()), new tara.dsl.Konos(), module.getName()).build();
+			if (stash == null) {
+				return null;
+			} else return GraphLoader.loadGraph(stash).graph();
+		} else return GraphLoader.loadGraph().graph();
 	}
 
 }
