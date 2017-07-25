d335dd1abcb9672423facab4af9eb219db042b7b
Mario Caballero Ramírez <mariocaballero@siani.es>
2017-07-25 12:11:15 +0100

    Added souls access support for box

--- server/src/io/intino/konos/server/activity/displays/DisplayRepository.java
+++ server/src/io/intino/konos/server/activity/displays/DisplayRepository.java
@@ -1,8 +1,10 @@
 package io.intino.konos.server.activity.displays;
 
+import java.util.List;
 import java.util.function.Consumer;
 
 public interface DisplayRepository {
+    List<Display> getAll();
     <T extends Display> T get(String id);
     <T extends Display> void register(T display);
     void addRegisterDisplayListener(Consumer<Display> consumer);
