77836a5db3f3cfd67ec7f44409381372badc5df2
Octavio Roncal <octavioroncal@monentia.es>
2017-08-02 11:33:20 +0100

    updated ness generated code. update ness library. coherence

---
+++ ide/builder/src/io/intino/konos/builder/codegeneration/datalake/NessTanksTemplate.java
@@ -0,0 +1,37 @@
+package io.intino.konos.builder.codegeneration.datalake;
+
+import org.siani.itrules.*;
+
+import java.util.Locale;
+
+import static org.siani.itrules.LineSeparator.*;
+
+public class NessTanksTemplate extends Template {
+
+	protected NessTanksTemplate(Locale locale, LineSeparator separator) {
+		super(locale, separator);
+	}
+
+	public static Template create() {
+		return new NessTanksTemplate(Locale.ENGLISH, LF).define();
+	}
+
+	public Template define() {
+		add(
+			rule().add((condition("type", "tanks"))).add(literal("package ")).add(mark("package", "validPackage")).add(literal(";\n\nimport io.intino.konos.jms.TopicConsumer;\nimport io.intino.konos.jms.TopicProducer;\nimport io.intino.konos.jms.Consumer;\nimport io.intino.konos.jms.Bus;\nimport org.apache.activemq.ActiveMQConnection;\nimport org.apache.activemq.advisory.DestinationListener;\nimport org.apache.activemq.advisory.DestinationSource;\nimport org.apache.activemq.command.ActiveMQTopic;\nimport ")).add(mark("package", "validPackage")).add(literal(".")).add(mark("box", "firstUpperCase")).add(literal("Box;\nimport ")).add(mark("package", "validPackage")).add(literal(".")).add(mark("box", "firstUpperCase")).add(literal("Configuration;\n")).add(expression().add(mark("tankImport"))).add(literal("\nimport javax.jms.JMSException;\nimport javax.jms.Message;\nimport java.util.ArrayList;\nimport java.util.List;\nimport java.util.logging.Level;\nimport java.util.logging.Logger;\n\npublic class NessTanks {\n\tprivate static Logger logger = Logger.getGlobal();\n\t")).add(mark("tank", "field").multiple("\n")).add(literal("\n\tprivate ")).add(mark("box", "firstUpperCase")).add(literal("Configuration configuration;\n\tprivate ")).add(mark("box", "firstUppercase")).add(literal("Box box;\n\n\tpublic static void registerTanks(")).add(mark("box", "firstUppercase")).add(literal("Box box) {\n\t\t")).add(mark("tank", "subscribe").multiple("\n")).add(literal("\n\t\t")).add(mark("tank", "assign").multiple("\n")).add(literal("\n\t}\n\n\tprivate static class Subscriptor implements Consumer {\n\n\t\tprivate final ")).add(mark("box", "firstUpperCase")).add(literal("Box box;\n\t\tprivate final String tank;\n\n\t\tSubscriptor(")).add(mark("box", "firstUpperCase")).add(literal("Box box, String tank) {\n\t\t\tthis.box = box;\n\t\t\tthis.tank = tank;\n\t\t}\n\n\t\tpublic void consume(Message message) {\n\t\t\tif (isRegisterOnly(message)) return;\n\t\t\tString text = Consumer.textFrom(message);\n\t\t\t")).add(mark("tank", "select").multiple("\nelse ")).add(literal("\n\t\t}\n\t}\n\n\t")).add(mark("tank", "getter").multiple("\n\n")).add(literal("\n\n\tprivate static boolean isRegisterOnly(Message message) {\n\t\ttry {\n\t\t\treturn message.getBooleanProperty(io.intino.konos.datalake.Ness.REGISTER_ONLY);\n\t\t} catch (JMSException e) {\n\t\t\treturn false;\n\t\t}\n\t}\n}")),
+			rule().add((condition("type", "tank")), (condition("trigger", "field"))).add(literal("private static io.intino.konos.datalake.Ness.Tank ")).add(mark("name")).add(literal(";")),
+			rule().add((condition("type", "tank")), (condition("trigger", "assign"))).add(mark("name")).add(literal(" = box.datalake().tank(")).add(mark("messageType", "format")).add(literal(");")),
+			rule().add((condition("type", "tank")), (condition("trigger", "getter"))).add(literal("public static io.intino.konos.datalake.Ness.Tank ")).add(mark("name")).add(literal("() {\n\treturn NessTanks.")).add(mark("name")).add(literal(";\n}")),
+			rule().add((condition("type", "tank")), (condition("trigger", "subscribe"))).add(literal("new TopicConsumer(box.datalake().session(), ")).add(mark("name")).add(literal(").listen(new Subscriptor(box, \"flow.\" + ")).add(mark("name")).add(literal("), box.configuration().nessConfiguration().clientID + \"-")).add(mark("name")).add(literal("\");")),
+			rule().add((condition("type", "custom")), (condition("trigger", "replace"))).add(literal(".replace(\"{")).add(mark("value")).add(literal("}\", ")).add(mark("value", "validname", "firstLowerCase")).add(literal(")")),
+			rule().add((condition("trigger", "formatMessage"))).add(literal("\"")).add(mark("name")).add(literal("\"")).add(expression().add(mark("custom", "customType").multiple(""))),
+			rule().add((condition("trigger", "customType"))).add(literal(".replace(\"{")).add(mark("value")).add(literal("}\", box.configuration().")).add(mark("conf")).add(literal("Configuration().")).add(mark("value", "validname", "firstLowerCase")).add(literal(")")),
+			rule().add((condition("trigger", "format"))).add(literal("\"")).add(mark("name")).add(literal("\"")).add(expression().add(mark("custom").multiple(""))),
+			rule().add((condition("trigger", "custom"))).add(literal(".replace(\"{")).add(mark("value")).add(literal("}\", box.configuration().")).add(mark("conf")).add(literal("Configuration().")).add(mark("value", "validname", "firstLowerCase")).add(literal(")")),
+			rule().add((condition("trigger", "tank")), (condition("trigger", "select"))).add(literal("if (")).add(mark("name")).add(literal(".name().equalsIgnoreCase(tank)) {\n\t")).add(mark("name", "snakeCaseToCamelCase", "FirstUpperCase")).add(literal("MessageHandler tank = new ")).add(mark("name", "snakeCaseToCamelCase", "FirstUpperCase")).add(literal("MessageHandler();\n\ttank.box = box;\n\ttank.message = io.intino.ness.Inl.load(text).get(0);\n\ttank.execute();\n}")),
+			rule().add((condition("trigger", "replace"))).add(literal(".replace(\"{")).add(mark("value")).add(literal("}\", configuration().")).add(mark("conf", "firstLowerCase")).add(literal("Configuration.")).add(mark("value", "validname", "firstLowerCase")).add(literal(")")),
+			rule().add((condition("trigger", "tankImport"))).add(literal("import ")).add(mark("value", "validPackage")).add(literal(".messagehandlers.*;"))
+		);
+		return this;
+	}
+}
