12177b197e2b6a966444a0fc22e50a36014a4624
Octavio Roncal <octavioroncal@monentia.es>
2017-08-16 10:33:24 +0100

    improved jmx code generation

--- ide/builder/src/io/intino/konos/builder/codegeneration/datalake/GraphProviderTemplate.java
+++ ide/builder/src/io/intino/konos/builder/codegeneration/datalake/GraphProviderTemplate.java
@@ -18,7 +18,7 @@ public class GraphProviderTemplate extends Template {
 
 	public Template define() {
 		add(
-			rule().add((condition("type", "operations"))).add(literal("package ")).add(mark("package", "validPackage")).add(literal(".ness;\n\nimport ")).add(mark("package", "validPackage")).add(literal(".")).add(mark("box", "FirstUpperCase")).add(literal("Box;\nimport io.intino.tara.magritte.Graph;\nimport io.intino.tara.magritte.Store;\n\nclass GraphProvider {\n\n\tprivate final ")).add(mark("box", "FirstUpperCase")).add(literal("Box box;\n\n\tGraphProvider(")).add(mark("box", "FirstUpperCase")).add(literal("Box box) {\n\t\tthis.box = box;\n\t}\n\n\tGraph graph() {\n\t\treturn null;\n\t\t//return box.graph();\n\t}\n\n\tStore store() {\n\t\treturn null;\n\t\t//return new SumusStore(new File(\"store\"));\n\t}\n\n\tvoid saveGraph(Graph graph) {\n\t\tgraph.saveAll(\"Model\");\n\t}\n}"))
+			rule().add((condition("type", "operations"))).add(literal("package ")).add(mark("package", "validPackage")).add(literal(".ness;\n\nimport ")).add(mark("package", "validPackage")).add(literal(".")).add(mark("box", "FirstUpperCase")).add(literal("Box;\nimport io.intino.tara.magritte.Graph;\nimport io.intino.tara.magritte.Store;\n\nclass GraphProvider {\n\n\tprivate final ")).add(mark("box", "FirstUpperCase")).add(literal("Box box;\n\n\tGraphProvider(")).add(mark("box", "FirstUpperCase")).add(literal("Box box) {\n\t\tthis.box = box;\n\t}\n\n\tStore store() {\n\t\treturn null;\n\t\t//return new FileSystemStore(new File(\"store\"));\n\t}\n\n\tvoid saveGraph(Graph graph) {\n\t\tgraph.saveAll(\"Model\");\n\t}\n}"))
 		);
 		return this;
 	}
