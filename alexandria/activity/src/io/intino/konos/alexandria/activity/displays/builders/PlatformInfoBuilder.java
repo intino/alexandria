1387ac10bbc0fd17f45a20dd8b18d4338e88bf61
Mario Caballero Ramírez <mariocaballero@siani.es>
2017-12-14 14:16:36 +0000

    Refactored logo and favicon from url to base 64 string

--- alexandria/activity/src/io/intino/konos/alexandria/activity/displays/builders/PlatformInfoBuilder.java
+++ alexandria/activity/src/io/intino/konos/alexandria/activity/displays/builders/PlatformInfoBuilder.java
@@ -1,46 +1,20 @@
 package io.intino.konos.alexandria.activity.displays.builders;
 
-import com.sun.org.apache.xml.internal.security.utils.Base64;
 import io.intino.konos.alexandria.activity.model.Settings;
 import io.intino.konos.alexandria.activity.schemas.PlatformInfo;
-
-import java.io.IOException;
-import java.net.URL;
-
-import static org.apache.tika.io.IOUtils.toByteArray;
 
 public class PlatformInfoBuilder {
 
     public static PlatformInfo build(Settings settings) {
         PlatformInfo info = new PlatformInfo().title(settings.title())
                 .subtitle(settings.subtitle())
-                .logo(logo(settings))
-                .favicon(favicon(settings));
+                .logo(settings.logo())
+                .favicon(settings.favicon());
 
         if (settings.authServiceUrl() != null)
             info.authServiceUrl(settings.authServiceUrl().toString());
 
         return info;
-    }
-
-    private static String logo(Settings settings) {
-        try {
-            return toBase64(settings.logo());
-        } catch (Exception e) {
-            return "";
-        }
-    }
-
-    private static String favicon(Settings settings) {
-        try {
-            return toBase64(settings.favicon());
-        } catch (Exception e) {
-            return "";
-        }
-    }
-
-    private static String toBase64(URL resource) throws IOException {
-        return "data:image/png;base64," + Base64.encode(toByteArray(resource.openStream()));
     }
 
 }
