cae89195c5a5ed11ba4ca12ac7aee09461a42b04
Mario Caballero Ramírez <mariocaballero@siani.es>
2017-12-15 12:37:58 +0000

    Refactoring stamps

--- alexandria/activity/src/io/intino/konos/alexandria/activity/model/mold/stamps/CatalogLink.java
+++ alexandria/activity/src/io/intino/konos/alexandria/activity/model/mold/stamps/CatalogLink.java
@@ -5,23 +5,8 @@ import io.intino.konos.alexandria.activity.model.Catalog;
 import io.intino.konos.alexandria.activity.model.mold.Stamp;
 
 public class CatalogLink extends Stamp<String> {
-	private Value<String> title;
 	private Catalog catalog;
 	private Filter filter;
-
-	public String title(Item item, String username) {
-		if (item == null) return null;
-		return objectTitle(item.object(), username);
-	}
-
-	public String objectTitle(Object object, String username) {
-		return title != null ? title.value(object, username) : null;
-	}
-
-	public CatalogLink title(Value<String> title) {
-		this.title = title;
-		return this;
-	}
 
 	public Catalog catalog() {
 		return this.catalog;
@@ -32,11 +17,11 @@ public class CatalogLink extends Stamp<String> {
 		return this;
 	}
 
-	public boolean filter(Item source, Item target) {
+	public boolean filter(Item source, Item target, String username) {
 		if (filter == null) return true;
 		if (source == null && target == null) return true;
 		if (source == null || target == null) return false;
-		return filter.filter(source.object(), target.object());
+		return filter.filter(source.object(), target.object(), username);
 	}
 
 	public CatalogLink filter(Filter filter) {
@@ -46,10 +31,10 @@ public class CatalogLink extends Stamp<String> {
 
 	@Override
 	public String objectValue(Object object, String username) {
-		return null;
+		return value() != null ? value().value(object, username) : null;
 	}
 
 	public interface Filter {
-		boolean filter(Object sourceObject, Object targetObject);
+		boolean filter(Object sourceObject, Object targetObject, String username);
 	}
 }
--- alexandria/activity/src/io/intino/konos/alexandria/activity/model/mold/stamps/EmbeddedCatalog.java
+++ alexandria/activity/src/io/intino/konos/alexandria/activity/model/mold/stamps/EmbeddedCatalog.java
@@ -1,24 +1,15 @@
 package io.intino.konos.alexandria.activity.model.mold.stamps;
 
 import io.intino.konos.alexandria.activity.displays.AlexandriaCatalog;
+import io.intino.konos.alexandria.activity.model.Catalog;
 import io.intino.konos.alexandria.activity.model.Element;
 import io.intino.konos.alexandria.activity.model.Item;
-import io.intino.konos.alexandria.activity.model.Catalog;
 import io.intino.konos.alexandria.activity.model.mold.Stamp;
 
 public class EmbeddedCatalog extends Stamp<String> {
 	private Catalog catalog;
+	private AlexandriaCatalog display;
 	private Filter filter;
-	private CatalogDisplayBuilder displayBuilder;
-
-	public Catalog catalog() {
-		return this.catalog;
-	}
-
-	public EmbeddedCatalog catalog(Catalog catalog) {
-		this.catalog = catalog;
-		return this;
-	}
 
 	public boolean filter(Element context, Item target, Item item) {
 		if (filter == null) return true;
@@ -32,13 +23,20 @@ public class EmbeddedCatalog extends Stamp<String> {
 		return this;
 	}
 
-	public AlexandriaCatalog display() {
-		return displayBuilder != null ? displayBuilder.display(catalog) : null;
+	public Catalog catalog() {
+		return catalog;
 	}
 
-	public EmbeddedCatalog displayBuilder(CatalogDisplayBuilder builder) {
-		this.displayBuilder = builder;
-		return this;
+	public void catalog(Catalog catalog) {
+		this.catalog = catalog;
+	}
+
+	public AlexandriaCatalog display() {
+		return display;
+	}
+
+	public void display(AlexandriaCatalog display) {
+		this.display = display;
 	}
 
 	@Override
@@ -50,7 +48,4 @@ public class EmbeddedCatalog extends Stamp<String> {
 		boolean filter(Element context, Object target, Object object);
 	}
 
-	public interface CatalogDisplayBuilder {
-		<CD extends AlexandriaCatalog> CD display(Catalog catalog);
-	}
 }
