fde36e578de4c7dd227b8e913826fdb060612426
Mario Caballero Ramírez <mariocaballero@siani.es>
2018-01-02 09:00:01 +0000

    Fix bug rendering hidden blocks

--- alexandria/activity/src/io/intino/konos/alexandria/activity/displays/AlexandriaStamp.java
+++ alexandria/activity/src/io/intino/konos/alexandria/activity/displays/AlexandriaStamp.java
@@ -1,9 +1,11 @@
 package io.intino.konos.alexandria.activity.displays;
 
+import io.intino.konos.alexandria.activity.displays.providers.AlexandriaStampProvider;
 import io.intino.konos.alexandria.activity.model.Item;
 
 public class AlexandriaStamp<N extends AlexandriaDisplayNotifier> extends AlexandriaDisplay<N> {
     private Item item;
+    private AlexandriaStampProvider provider;
 
     public Item item() {
         return item;
@@ -12,5 +14,18 @@ public class AlexandriaStamp<N extends AlexandriaDisplayNotifier> extends Alexan
     public AlexandriaStamp item(Item item) {
         this.item = item;
         return this;
+    }
+
+    public AlexandriaStamp provider(AlexandriaStampProvider provider) {
+        this.provider = provider;
+        return this;
+    }
+
+    public AlexandriaStamp embeddedDisplay(String stamp) {
+        return provider.embeddedDisplay(stamp);
+    }
+
+    public AlexandriaCatalog embeddedCatalog(String stamp) {
+        return provider.embeddedCatalog(stamp);
     }
 }
