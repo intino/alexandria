322abac954493968581739a378033d3d014fc0d0
Octavio Roncal <octavioroncal@monentia.es>
2018-02-14 14:52:47 +0000

    refactor

--- alexandria/core/src/io/intino/konos/alexandria/Inl.java
+++ alexandria/core/src/io/intino/konos/alexandria/Inl.java
@@ -3,8 +3,12 @@ package io.intino.konos.alexandria;
 
 import io.intino.konos.alexandria.schema.Deserializer;
 import io.intino.konos.alexandria.schema.Serializer;
+import io.intino.ness.inl.Loader;
+import io.intino.ness.inl.Message;
+import io.intino.ness.inl.MessageInputStream;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,33 +16,44 @@ import java.util.List;
 
 public class Inl {
 
-    public static String serialize(Object object) {
-        return Serializer.serialize(object).toInl();
-    }
+	public static String serialize(Object object) {
+		return Serializer.serialize(object).toInl();
+	}
 
-    public static Deserializer deserialize(InputStream is) {
-        return Deserializer.deserialize(is);
-    }
+	public static Deserializer deserialize(InputStream is) {
+		return Deserializer.deserialize(is);
+	}
 
-    public static Deserializer deserialize(String text) {
-        return deserialize(new ByteArrayInputStream(text.getBytes()));
-    }
+	public static Deserializer deserialize(String text) {
+		return deserialize(new ByteArrayInputStream(text.getBytes()));
+	}
 
-    public static <T> List<T> deserializeAll(InputStream is, Class<T> aClass) {
-        List<T> list = new ArrayList<>();
-        Deserializer deserialize = deserialize(is);
-        while (true) {
-            T object = deserialize.next(aClass);
-            if (object == null) break;
-            list.add(object);
+	public static <T> List<T> deserializeAll(InputStream is, Class<T> aClass) {
+		List<T> list = new ArrayList<>();
+		Deserializer deserialize = deserialize(is);
+		while (true) {
+			T object = deserialize.next(aClass);
+			if (object == null) break;
+			list.add(object);
 
-        }
-        return list;
-    }
+		}
+		return list;
+	}
 
-    public static <T> List<T> deserializeAll(String text, Class<T> aClass) {
-        return deserializeAll(new ByteArrayInputStream(text.getBytes()), aClass);
-    }
+	public static <T> List<T> deserializeAll(String text, Class<T> aClass) {
+		return deserializeAll(new ByteArrayInputStream(text.getBytes()), aClass);
+	}
+
+	public static List<Message> load(String text) {
+		ArrayList<Message> list = new ArrayList<>();
+		Message message;
+		try {
+			MessageInputStream inputStream = Loader.Inl.of(new ByteArrayInputStream(text.getBytes()));
+			while ((message = inputStream.next()) != null) list.add(message);
+		} catch (IOException ignored) {
+		}
+		return list;
+	}
 
 
 }
