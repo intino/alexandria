6ad176ea5e0582b7cd789c72702a2554e61c1f51
Octavio Roncal <octavioroncal@monentia.es>
2018-03-05 07:41:58 +0000

    new ness-accessor 1.0.7 updated templates

--- ness-accessor/src/io/intino/konos/datalake/Reflow.java
+++ ness-accessor/src/io/intino/konos/datalake/Reflow.java
@@ -1,9 +1,12 @@
 package io.intino.konos.datalake;
 
+import java.time.Instant;
+
 public class Reflow implements java.io.Serializable {
 
 	private Integer blockSize = 0;
 	private java.util.List<String> tanks = new java.util.ArrayList<>();
+	private Instant from = Instant.MIN;
 
 	public Integer blockSize() {
 		return this.blockSize;
@@ -21,5 +24,14 @@ public class Reflow implements java.io.Serializable {
 	public Reflow tanks(java.util.List<String> tanks) {
 		this.tanks = tanks;
 		return this;
+	}
+
+	public Reflow from(Instant from) {
+		this.from = from;
+		return this;
+	}
+
+	public Instant from() {
+		return from;
 	}
 }
