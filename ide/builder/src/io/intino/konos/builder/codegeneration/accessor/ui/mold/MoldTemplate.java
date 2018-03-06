fbd7d9e2615741194291b62c3154a35ade4cc988
Mario Caballero Ramírez <mariocaballero@siani.es>
2018-03-06 13:45:18 +0000

    alexandria-activity: Added Mold Layout generation

--- ide/builder/src/io/intino/konos/builder/codegeneration/accessor/ui/mold/MoldTemplate.java
+++ ide/builder/src/io/intino/konos/builder/codegeneration/accessor/ui/mold/MoldTemplate.java
@@ -18,7 +18,7 @@ public class MoldTemplate extends Template {
 
 	public Template define() {
 		add(
-			rule().add((condition("trigger", "mold"))).add(literal("\n<link rel=\"import\" href=\"../../lib/polymer/polymer.html\">\n<link rel=\"import\" href=\"")).add(mark("name", "lowercase")).add(literal("/")).add(mark("name", "camelCaseToSnakeCase-layout")).add(literal(".html\">\n\n<dom-module id=\"")).add(mark("name", "camelCaseToSnakeCase")).add(literal("\">\n\n\t<template>\n\t\t<")).add(mark("name", "camelCaseToSnakeCase-layout")).add(literal(" item=\"[[item]]\" mode=\"[[mode]]\"></")).add(mark("name", "camelCaseToSnakeCase-layout")).add(literal(">\n\t</template>\n\n\t<script>\n\t\tPolymer({\n\t\t\tis: '")).add(mark("name", "camelCaseToSnakeCase")).add(literal("',\n\n\t\t\tproperties : {\n\t\t\t\titem : Object,\n\t\t\t\tmode : String\n\t\t\t}\n\t\t});\n\t</script>\n</dom-module>\n"))
+			rule().add((condition("type", "mold"))).add(literal("\n<link rel=\"import\" href=\"../../lib/polymer/polymer.html\">\n<link rel=\"import\" href=\"")).add(mark("name", "lowercase")).add(literal("/")).add(mark("name", "camelCaseToSnakeCase")).add(literal("-layout.html\">\n\n<dom-module id=\"")).add(mark("name", "CamelCaseToSnakeCase")).add(literal("\">\n\n\t<template>\n\t\t<")).add(mark("name", "camelCaseToSnakeCase")).add(literal("-layout item=\"[[item]]\" mode=\"[[mode]]\"></")).add(mark("name", "camelCaseToSnakeCase")).add(literal("-layout>\n\t</template>\n\n\t<script>\n\t\tPolymer({\n\t\t\tis: '")).add(mark("name", "camelCaseToSnakeCase")).add(literal("',\n\n\t\t\tproperties : {\n\t\t\t\titem : Object,\n\t\t\t\tmode : String\n\t\t\t}\n\t\t});\n\t</script>\n</dom-module>"))
 		);
 		return this;
 	}
