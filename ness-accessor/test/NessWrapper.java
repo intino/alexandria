b25cb094545fdea73adda7560eee3f129ea73e2c
Octavio Roncal <octavioroncal@monentia.es>
2018-05-17 09:38:28 +0100

    updated ness accessor with attachment support

--- ness-accessor/test/NessWrapper.java
+++ ness-accessor/test/NessWrapper.java
@@ -1,15 +1,28 @@
 import io.intino.konos.datalake.Datalake;
 import io.intino.konos.datalake.Ness;
-import io.intino.konos.datalake.jms.JMSTank;
+import io.intino.ness.inl.Message;
 import org.junit.Test;
+
+import java.time.Instant;
 
 public class NessWrapper {
 
 	@Test
 	public void shouldStartNess() throws Exception {
-		final Ness ness = new Ness("failover:(tcp://localhost:61616)","ness", "ness", "ness");
+		final Ness ness = new Ness("failover:(tcp://localhost:61616)", "ness", "ness", "ness");
 //		ness.start();
 		final Datalake.Tank tank = ness.add("");
 		Thread.sleep(10000);
+	}
+
+
+	@Test
+	public void sendAttachment() {
+		final Ness ness = new Ness("file://./temp/attach", "user", "password", "");
+		final Datalake.Tank tank = ness.add("happysense.dialog");
+		final Message message = new Message("dialog").set("name", "dialog1");
+		message.set("ts", Instant.now().toString());
+		message.set("value", "txt", "example".getBytes());
+		tank.drop(message);
 	}
 }
