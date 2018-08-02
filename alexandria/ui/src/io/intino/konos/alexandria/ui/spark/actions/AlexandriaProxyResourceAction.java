5d9ad8bd71eb3d178d57becfe9ba8e8fac9146ec
Mario Caballero Ramírez <mariocaballero@siani.es>
2018-08-02 12:04:42 +0100

    alexandria-ui: new proxy classes

--- alexandria/ui/src/io/intino/konos/alexandria/ui/spark/actions/AlexandriaProxyResourceAction.java
+++ alexandria/ui/src/io/intino/konos/alexandria/ui/spark/actions/AlexandriaProxyResourceAction.java
@@ -5,8 +5,8 @@ import java.net.URL;
 
 public abstract class AlexandriaProxyResourceAction extends AlexandriaResourceAction {
 
-	public AlexandriaProxyResourceAction(String uiServiceName) {
-		super(uiServiceName);
+	public AlexandriaProxyResourceAction() {
+		super(null);
 	}
 
 	@Override
