2af4ee4e477bcb9d603c90e4b022272db954d27b
Octavio Roncal <octavioroncal@monentia.es>
2018-09-12 09:41:56 +0100

    updated feeder version

--- ide/builder/src/io/intino/konos/builder/codegeneration/datalake/feeder/FeederTemplate.java
+++ ide/builder/src/io/intino/konos/builder/codegeneration/datalake/feeder/FeederTemplate.java
@@ -1,10 +1,11 @@
 package io.intino.konos.builder.codegeneration.datalake.feeder;
 
-import org.siani.itrules.*;
+import org.siani.itrules.LineSeparator;
+import org.siani.itrules.Template;
 
 import java.util.Locale;
 
-import static org.siani.itrules.LineSeparator.*;
+import static org.siani.itrules.LineSeparator.LF;
 
 public class FeederTemplate extends Template {
 
@@ -18,7 +19,7 @@ public class FeederTemplate extends Template {
 
 	public Template define() {
 		add(
-			rule().add((condition("type", "feeder"))).add(literal("package ")).add(mark("package", "validPackage")).add(literal(".ness.feeders;\n\nimport ")).add(mark("package", "validPackage")).add(literal(".")).add(mark("box", "firstUpperCase")).add(literal("Box;\n\nimport io.intino.ness.inl.Message;\nimport java.util.Arrays;\n\npublic class ")).add(mark("name", "FirstUpperCase")).add(literal("Feeder extends Abstract")).add(mark("name", "FirstUpperCase")).add(literal("Feeder {\n\tprivate final ")).add(mark("box", "FirstUpperCase")).add(literal("Box box;\n\n\tpublic ")).add(mark("name", "FirstUpperCase")).add(literal("Feeder(")).add(mark("box", "FirstUpperCase")).add(literal("Box box) {\n\t\tthis.box = box;\n\t}\n\n\t")).add(mark("sensor", "class")).add(literal("\n}")),
+			rule().add((condition("type", "feeder"))).add(literal("package ")).add(mark("package", "validPackage")).add(literal(".ness.feeders;\n\nimport ")).add(mark("package", "validPackage")).add(literal(".")).add(mark("box", "firstUpperCase")).add(literal("Box;\n\nimport io.intino.ness.inl.Message;\nimport java.util.Arrays;\n\npublic class ")).add(mark("name", "FirstUpperCase")).add(literal(" extends Abstract")).add(mark("name", "FirstUpperCase")).add(literal("Feeder {\n\tprivate final ")).add(mark("box", "FirstUpperCase")).add(literal("Box box;\n\n\tpublic ")).add(mark("name", "FirstUpperCase")).add(literal("Feeder(")).add(mark("box", "FirstUpperCase")).add(literal("Box box) {\n\t\tthis.box = box;\n\t}\n\n\t")).add(mark("sensor", "class")).add(literal("\n}")),
 			rule().add((condition("type", "sensor")), (condition("trigger", "class"))).add(literal("public static class ")).add(mark("name", "FirstUpperCase")).add(literal("Sensor extends Abstract")).add(mark("name", "FirstUpperCase")).add(literal("Sensor {\n\n\tpublic ")).add(mark("name", "FirstUpperCase")).add(literal("Sensor() {\n\t}\n\n\t")).add(mark("parent", "get")).add(literal("\n\n\t")).add(mark("parent", "methods")).add(literal("\n}")),
 			rule().add((condition("type", "poll")), (condition("trigger", "get"))).add(literal("public Message get(Object... args) {\n\tString option = null; //TODO\n\treturn get(option, args);\n}")),
 			rule().add((condition("trigger", "get"))).add(literal("public Message get(Object... args) {\n\treturn null;\n}")),
