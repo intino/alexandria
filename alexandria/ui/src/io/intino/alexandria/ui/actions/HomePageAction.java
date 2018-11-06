7835b3e4f19d5b8d72d23cb05d74334ac6be3ea1
Mario Caballero Ramírez <mariocaballero@siani.es>
2018-11-06 19:24:24 +0000

    alexandria-ui: several changes to adapt to new konos approach

--- alexandria/ui/src/io/intino/alexandria/ui/actions/HomePageAction.java
+++ alexandria/ui/src/io/intino/alexandria/ui/actions/HomePageAction.java
@@ -1,14 +1,12 @@
 package io.intino.alexandria.ui.actions;
 
-import io.intino.alexandria.ui.UiBox;
+import io.intino.alexandria.ui.UiFrameworkBox;
 import io.intino.alexandria.ui.displays.AlexandriaDesktop;
 import io.intino.alexandria.ui.displays.Soul;
 import io.intino.alexandria.ui.services.push.UIClient;
 
 public class HomePageAction extends AbstractHomePageAction {
-
-	public UiBox box;
-
+	public UiFrameworkBox box;
 
 	public Soul prepareSoul(UIClient client) {
 	    return new Soul(session) {
