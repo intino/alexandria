6cff8703ec2356924da31664ae9b1f480d06433d
Octavio Roncal <octavioroncal@monentia.es>
2018-11-09 10:02:24 +0000

    updated datalake template updated tcp datalake

--- ness-accessor/src/io/intino/alexandria/nessaccessor/tcp/TcpDatalake.java
+++ ness-accessor/src/io/intino/alexandria/nessaccessor/tcp/TcpDatalake.java
@@ -1,10 +1,6 @@
 package io.intino.alexandria.nessaccessor.tcp;
 
-import io.intino.alexandria.inl.Message;
-import io.intino.alexandria.jms.MessageFactory;
-import io.intino.alexandria.jms.TopicProducer;
 import io.intino.alexandria.logger.Logger;
-import io.intino.alexandria.nessaccessor.MessageTranslator;
 import io.intino.alexandria.zim.ZimReader;
 import io.intino.alexandria.zim.ZimStream;
 import io.intino.ness.core.Blob;
@@ -14,22 +10,18 @@ import org.apache.activemq.ActiveMQSession;
 
 import javax.jms.JMSException;
 import javax.jms.Session;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.stream.Stream;
 
 import static io.intino.ness.core.Blob.Type.event;
 import static javax.jms.Session.AUTO_ACKNOWLEDGE;
 
 public class TcpDatalake implements Datalake {
-	private static final String SERVICE_NESS_ADMIN = "service.ness.admin";
 	private final Connection connection;
-	private final Session session;
-	private final Map<String, TopicProducer> producers = new HashMap<>();
+	private TCPEventStore tcpEventStore;
 
 	public TcpDatalake(String uri, String username, String password, String clientId) {
 		this.connection = new Connection(uri, username, password, clientId, onClose());
-		this.session = this.connection.session;
+		this.tcpEventStore = new TCPEventStore(connection.session);
 	}
 
 	@Override
@@ -39,7 +31,7 @@ public class TcpDatalake implements Datalake {
 
 	@Override
 	public EventStore eventStore() {
-		return new TCPEventStore(connection.session);
+		return tcpEventStore;
 	}
 
 	@Override
@@ -49,40 +41,12 @@ public class TcpDatalake implements Datalake {
 
 	@Override
 	public void seal() {
-		producer(SERVICE_NESS_ADMIN).produce(MessageFactory.createMessageFor("seal"));
+		this.tcpEventStore.seal();
 	}
 
 	@Override
 	public void push(Stream<Blob> blobs) {
-		blobs.filter(b -> b.type().equals(event)).forEach(b -> send(read(b), b.name()));
-	}
-
-	public void feed(String tank, Message... messages) {
-		for (Message message : messages) send(message, feedTopicOf(tank));
-	}
-
-	private void send(ZimStream stream, String blob) {
-		while (stream.hasNext()) send(stream.next(), putTopicOf(blob));
-	}
-
-	private String feedTopicOf(String name) {
-		return "feed." + tankName(name);
-	}
-
-	private String putTopicOf(String name) {
-		return "put." + tankName(name);
-	}
-
-	private String tankName(String name) {
-		return name.substring(0, name.indexOf("-"));
-	}
-
-	private void send(Message message, String topic) {
-		if (connection.session == null || ((ActiveMQSession) session).isClosed()) {
-			Logger.error("Session closed");
-			return;
-		}
-		producer(topic).produce(MessageTranslator.fromInlMessage(message));
+		blobs.filter(b -> b.type().equals(event)).forEach(b -> tcpEventStore.put(read(b), b.name()));
 	}
 
 	private ZimStream read(Blob b) {
@@ -90,20 +54,9 @@ public class TcpDatalake implements Datalake {
 	}
 
 	private CallBack onClose() {
-		return () -> producers.values().forEach(p -> {
+		return () -> tcpEventStore.producers().forEach(p -> {
 			if (!p.isClosed()) p.close();
 		});
-	}
-
-	private TopicProducer producer(String topic) {
-		try {
-			if (this.producers.containsKey(topic) && !this.producers.get(topic).isClosed()) return this.producers.get(topic);
-			this.producers.put(topic, new TopicProducer(session, topic));
-			return producers.get(topic);
-		} catch (JMSException e) {
-			Logger.error(e);
-			return null;
-		}
 	}
 
 	public static class Connection implements Datalake.Connection {
