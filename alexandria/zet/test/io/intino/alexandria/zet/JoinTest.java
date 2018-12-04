fc0c856e4351ebe54f3a34c82b6807c47dbc97b6
Octavio Roncal <octavioroncal@monentia.es>
2018-12-04 10:10:04 +0000

    updated zet and ness accessor

--- alexandria/zet/test/io/intino/alexandria/zet/JoinTest.java
+++ alexandria/zet/test/io/intino/alexandria/zet/JoinTest.java
@@ -12,7 +12,7 @@ public class JoinTest {
 
 	@Test
 	public void should_read_three_sorted_files_without_duplicates() {
-		ZetStream.Join join = new ZetStream.Join(asList(
+		ZetStream.Merge merge = new ZetStream.Merge(asList(
 				new ZetReader(new File("test-res/testsets/norep1.zet")),
 				new ZetReader(new File("test-res/testsets/norep2.zet")),
 				new ZetReader(new File("test-res/testsets/norep3.zet"))));
@@ -20,11 +20,11 @@ public class JoinTest {
 		int count = 0;
 
 		for (int i = 0; i < 1000; i++) {
-			assertEquals(i, join.next());
+			assertEquals(i, merge.next());
 			count++;
 		}
 
 		Assert.assertEquals(1000, count);
-		assertEquals(-1, join.next());
+		assertEquals(-1, merge.next());
 	}
 }
