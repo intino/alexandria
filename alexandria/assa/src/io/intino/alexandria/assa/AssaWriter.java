d571a09bb8780612284055d278c017bfdb5a8fb5
jevora <jose.evora@siani.es>
2018-12-11 09:19:22 +0000

    modified assa writer to use assa builder

--- alexandria/assa/src/io/intino/alexandria/assa/AssaWriter.java
+++ alexandria/assa/src/io/intino/alexandria/assa/AssaWriter.java
@@ -1,75 +1,72 @@
 package io.intino.alexandria.assa;
 
-import io.intino.alexandria.logger.Logger;
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
 
-import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Stream;
-
-import static java.util.Comparator.comparing;
-
-public class AssaWriter {
+public class AssaWriter<T extends Serializable> {
 	private final File file;
-	private final Map<Object, Integer> objects;
+//	private final Map<Object, Integer> objects;
 
 	public AssaWriter(File file) {
 		this.file = file;
-		this.objects = new HashMap<>();
 	}
 
-	public void save(String name, AssaStream input) throws IOException {
-		try (DataOutputStream output = output()) {
-			output.writeUTF(name);
-			output.writeInt(input.size());
-			writeItems(input, output);
-			output.writeInt(objects.size());
-			ObjectOutputStream objectOutputStream = objectOutputStream(output);
-			writeObjects(objectOutputStream);
-		}
-	}
-
-	private DataOutputStream output() throws FileNotFoundException {
-		return new DataOutputStream(new BufferedOutputStream(new FileOutputStream(file)));
-	}
-
-	private void writeItems(AssaStream input, DataOutputStream output) throws IOException {
+	public void save(String name, AssaStream<T> input) {
+		AssaBuilder<T> builder = new AssaBuilder<>(name);
 		while (input.hasNext()) {
-			AssaStream.Item item = input.next();
-			if (item == null) break;
-			output.writeLong(item.key());
-			output.writeInt(map(item.object()));
+			AssaStream.Item<T> next = input.next();
+			builder.put(next.key(), next.object());
 		}
-	}
-
-	private void writeObjects(ObjectOutputStream output) {
-		objects().forEach(o -> writeObject(output, o));
-	}
-
-	private void writeObject(ObjectOutputStream output, Object object) {
 		try {
-			output.writeObject(object);
+			builder.save(file);
 		} catch (IOException e) {
-			Logger.error(e);
+			e.printStackTrace();
 		}
 	}
+	// TODO ver con JJ
 
-	private int map(Object object) {
-		if (objects.containsKey(object)) return objects.get(object);
-		objects.put(object, objects.size());
-		return objects.size() - 1;
-	}
-
-	private Stream<Object> objects() {
-		return objects.entrySet()
-				.stream()
-				.sorted(comparing(Map.Entry::getValue))
-				.map(Map.Entry::getKey);
-	}
-
-	private ObjectOutputStream objectOutputStream(DataOutputStream output) throws IOException {
-		return new ObjectOutputStream(output);
-	}
+//	private DataOutputStream output() throws FileNotFoundException {
+//		return new DataOutputStream(new BufferedOutputStream(new FileOutputStream(file)));
+//	}
+//
+//	private void writeItems(AssaStream input, DataOutputStream output) throws IOException {
+//		while (input.hasNext()) {
+//			AssaStream.Item item = input.next();
+//			if (item == null) break;
+//			output.writeLong(item.key());
+//			output.writeInt(map(item.object()));
+//		}
+//	}
+//
+//	private void writeObjects(ObjectOutputStream output) {
+//		objects().forEach(o -> writeObject(output, o));
+//	}
+//
+//	private void writeObject(ObjectOutputStream output, Object object) {
+//		try {
+//			output.writeObject(object);
+//		} catch (IOException e) {
+//			Logger.error(e);
+//		}
+//	}
+//
+//	private int map(Object object) {
+//		if (objects.containsKey(object)) return objects.get(object);
+//		objects.put(object, objects.size());
+//		return objects.size() - 1;
+//	}
+//
+//	private Stream<Object> objects() {
+//		return objects.entrySet()
+//				.stream()
+//				.sorted(comparing(Map.Entry::getValue))
+//				.map(Map.Entry::getKey);
+//	}
+//
+//	private ObjectOutputStream objectOutputStream(DataOutputStream output) throws IOException {
+//		return new ObjectOutputStream(output);
+//	}
 
 
 }
