aa704806cdbe19f5bd103931eca99861cc7a5e96
Octavio Roncal <octavioroncal@monentia.es>
2018-12-20 09:55:29 +0000

    updated builder

--- ide/builder/src/io/intino/konos/builder/codegeneration/datalake/NessJMXOperationsRenderer.java
+++ ide/builder/src/io/intino/konos/builder/codegeneration/datalake/NessJMXOperationsRenderer.java
@@ -14,29 +14,32 @@ public class NessJMXOperationsRenderer {
 	private File src;
 	private final String packageName;
 	private final String boxName;
+	private final boolean hasGraph;
 
-	public NessJMXOperationsRenderer(File gen, File src, String packageName, String boxName) {
+	public NessJMXOperationsRenderer(File gen, File src, String packageName, String boxName, boolean hasGraph) {
 		this.gen = gen;
 		this.src = src;
 		this.packageName = packageName;
 		this.boxName = boxName;
+		this.hasGraph = hasGraph;
 	}
 
 	public void execute() {
-		Frame frame = new Frame("operations", "graph").
+		Frame frame = new Frame("operations").
 				addSlot("package", packageName).
 				addSlot("box", boxName);
+		if (hasGraph) frame.addTypes("graph");
 		writeFrame(new File(gen, "datalake"), "NessOperations", operationsTemplate().format(frame));
 		writeFrame(new File(gen, "datalake"), "NessOperationsMBean", operationsTemplate().format(frame.addTypes("interface")));
 		if (!Commons.javaFile(new File(src, "datalake"), "ReflowAssistant").exists())
-			writeFrame(new File(src, "datalake"), "ReflowAssistant", providerTemplate().format(frame));
+			writeFrame(new File(src, "datalake"), "ReflowAssistant", assistantTemplate().format(frame));
 	}
 
 	private Template operationsTemplate() {
 		return Formatters.customize(NessJMXOperationsTemplate.create());
 	}
 
-	private Template providerTemplate() {
+	private Template assistantTemplate() {
 		return Formatters.customize(ReflowAssistantTemplate.create());
 	}
 }
