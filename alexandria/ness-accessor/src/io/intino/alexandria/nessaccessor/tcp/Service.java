2fbcbc7c80cae46ed77e136df52dde11aa1888be
Octavio Roncal <octavioroncal@monentia.es>
2019-01-15 14:40:11 +0000

    updated dependencies. ness-accessor new version

---
+++ alexandria/ness-accessor/src/io/intino/alexandria/nessaccessor/tcp/Service.java
@@ -0,0 +1,43 @@
+package io.intino.alexandria.nessaccessor.tcp;
+
+import io.intino.alexandria.jms.MessageFactory;
+import io.intino.alexandria.jms.TopicProducer;
+import io.intino.alexandria.logger.Logger;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
+
+import static io.intino.alexandria.jms.Consumer.textFrom;
+
+public abstract class Service {
+	protected final Session session;
+
+	public Service(Session session) {
+		this.session = session;
+	}
+
+	String request(String query) {
+		try {
+			TopicProducer producer = newProducer();
+			if (producer == null) return "";
+			Message message = MessageFactory.createMessageFor(query);
+			message.setJMSReplyTo(this.session.createTemporaryQueue());
+			producer.produce(message);
+			Message response = session.createConsumer(message.getJMSReplyTo()).receive(1000);
+			return response == null ? "" : textFrom(response);
+		} catch (JMSException e) {
+			Logger.error(e);
+			return "";
+		}
+	}
+
+	void send(String message) {
+		TopicProducer producer = newProducer();
+		if (producer == null) return;
+		producer.produce(MessageFactory.createMessageFor(message));
+		producer.close();
+	}
+
+	protected abstract TopicProducer newProducer();
+}
