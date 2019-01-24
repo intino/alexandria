622896afab6a55d8cba04ca9b6601194e4e3dc42
Octavio Roncal <octavioroncal@monentia.es>
2019-01-24 09:54:21 +0000

    updated logger. adapted modules to it. Fixed template errors

--- alexandria/logger/src/io/intino/alexandria/logger/Logger.java
+++ alexandria/logger/src/io/intino/alexandria/logger/Logger.java
@@ -1,62 +1,128 @@
 package io.intino.alexandria.logger;
 
-import org.slf4j.LoggerFactory;
-
-import static org.slf4j.Logger.ROOT_LOGGER_NAME;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.logging.LogRecord;
 
 public class Logger {
+	private static PrintStream out = System.out;
+	private static PrintStream err = System.err;
+	private static List<String> excludedPackages = new ArrayList<>();
+	private static Set<Level> excludedLevels = new HashSet<>();
+	private static String pattern = "[%level]\nts: %date\nsource: %C\nmessage: %m\n";
 
-	private static org.slf4j.Logger logger = LoggerFactory.getLogger(ROOT_LOGGER_NAME);
-
-	private static org.slf4j.Logger loggerOf(String namespace) {
-		return LoggerFactory.getLogger(namespace);
+	static {
+		excludedLevels.add(Level.TRACE);
 	}
 
 	public static void trace(String message) {
-		logger.trace(message);
+		if (isExcluded() || excludedLevels.contains(Level.TRACE)) return;
+		out.print(format(Level.TRACE, message));
 	}
 
 	public static void debug(String message) {
-		logger.debug(message);
+		if (isDebugging()) out.print(format(Level.DEBUG, message));
 	}
 
 	public static void info(String message) {
-		logger.info(message);
+		if (isExcluded() || excludedLevels.contains(Level.INFO)) return;
+		out.print(format(Level.INFO, message));
 	}
 
 	public static void warn(String message) {
-		logger.warn(message);
+		if (isExcluded() || excludedLevels.contains(Level.WARN)) return;
+		out.print(format(Level.WARN, message));
 	}
 
 	public static void error(String message) {
-		logger.error(message);
+		if (isExcluded() || excludedLevels.contains(Level.ERROR)) return;
+		err.print(format(Level.ERROR, message));
 	}
 
 	public static void error(Throwable e) {
-		logger.error(e.getMessage(), e);
+		if (isExcluded() || excludedLevels.contains(Level.ERROR)) return;
+		err.print(format(e));
 	}
 
 	public static void error(String message, Throwable e) {
-		logger.error(message, e);
+		if (isExcluded() || excludedLevels.contains(Level.ERROR)) return;
+		err.print(format(message, e));
 	}
 
-	public static void trace(String namespace, String message) {
-		loggerOf(namespace).trace(message);
+	public static void excludePackage(String aPackage) {
+		excludedPackages.add(aPackage);
 	}
 
-	public static void debug(String namespace, String message) {
-		loggerOf(namespace).debug(message);
+	public static void includePackage(String aPackage) {
+		excludedPackages.remove(aPackage);
 	}
 
-	public static void warn(String namespace, String message) {
-		loggerOf(namespace).warn(message);
+	public static void excludeLevel(Level level) {
+		excludedLevels.add(level);
 	}
 
-	public static void error(String namespace, String message) {
-		loggerOf(namespace).error(message);
+	public static void includeLevel(Level level) {
+		excludedLevels.remove(level);
 	}
 
-	public static void error(String namespace, String message, Object o) {
-		loggerOf(namespace).error(message, o);
+	static String format(LogRecord record) {
+		return pattern.replace("%level", record.getLevel().getName()).replace("%date", Instant.now().toString()).replace("%C", record.getSourceClassName() + ":" + record.getSourceMethodName()).replace("%m", formatMessage(record.getMessage(), record.getThrown()));
+	}
+
+	private static String format(String message, Throwable e) {
+		return pattern.replace("%level", Level.ERROR.name()).replace("%date", Instant.now().toString()).replace("%C", caller()).replace("%m", formatMessage(message, e));
+	}
+
+	private static String format(Throwable e) {
+		return pattern.replace("%level", Level.ERROR.name()).replace("%date", Instant.now().toString()).replace("%C", caller()).replace("%m", formatMessage(e));
+	}
+
+	private static String format(Level level, String message) {
+		return pattern.replace("%level", level.name()).replace("%date", Instant.now().toString()).replace("%C", caller()).replace("%m", formatMessage(message));
+	}
+
+	private static String formatMessage(String message, Throwable e) {
+		if (message == null) return formatMessage(e);
+		return "\n\t" + formatMessage(message) + (e != null ? ("\n\t\n\tCaused by:" + formatMessage(e)) : "");
+	}
+
+	private static String formatMessage(String message) {
+		return !message.contains("\n") ? message : "\n\t" + message.replace("\n", "\n\t");
+	}
+
+	private static String formatMessage(Throwable e) {
+		StringWriter writer = new StringWriter();
+		e.printStackTrace(new PrintWriter(writer));
+		return "\n\t" + writer.toString().replace("\n", "\n\t");
+	}
+
+	private static String caller() {
+		StackTraceElement[] stElements = Thread.currentThread().getStackTrace();
+		for (int i = 1; i < stElements.length; i++) {
+			StackTraceElement ste = stElements[i];
+			if (!ste.getClassName().equals(Logger.class.getName()) && ste.getClassName().indexOf("java.lang.Thread") != 0) {
+				return ste.getClassName() + ":" + ste.getMethodName() + ":" + ste.getLineNumber();
+			}
+		}
+		return null;
+	}
+
+	private static boolean isDebugging() {
+		return java.lang.management.ManagementFactory.getRuntimeMXBean().getInputArguments().toString().indexOf("-agentlib:jdwp") > 0;
+	}
+
+	private static boolean isExcluded() {
+		String caller = caller();
+		return excludedPackages.stream().anyMatch(caller::startsWith);
+	}
+
+	public enum Level {
+		ERROR, WARN, INFO, DEBUG, TRACE
 	}
 }
