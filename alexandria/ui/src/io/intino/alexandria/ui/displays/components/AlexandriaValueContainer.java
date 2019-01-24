50cae34d98a313445c82fbb7740b44e94333fa24
Mario Caballero Ramírez <mariocaballero@siani.es>
2019-01-24 19:03:26 +0000

    -

--- alexandria/ui/src/io/intino/alexandria/ui/displays/components/AlexandriaValueContainer.java
+++ alexandria/ui/src/io/intino/alexandria/ui/displays/components/AlexandriaValueContainer.java
@@ -4,16 +4,21 @@ import io.intino.alexandria.core.Box;
 import io.intino.alexandria.ui.displays.AlexandriaComponent;
 import io.intino.alexandria.ui.displays.notifiers.AlexandriaComponentNotifier;
 
-public abstract class ValueContainer<DN extends AlexandriaComponentNotifier, T, B extends Box> extends AlexandriaComponent<DN, B> {
+public abstract class AlexandriaValueContainer<DN extends AlexandriaComponentNotifier, T, B extends Box> extends AlexandriaComponent<DN, B> {
     private T value;
 
-    public ValueContainer(B box) {
+    public AlexandriaValueContainer(B box) {
         super(box);
     }
 
     @Override
     protected void init() {
         super.init();
+        notifyValue(value);
+    }
+
+    @Override
+    public void refresh() {
         notifyValue(value);
     }
 
