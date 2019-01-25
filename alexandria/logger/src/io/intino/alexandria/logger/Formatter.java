085f4298e11868f1beae0e6ac4238be97fbd006e
Octavio Roncal <octavioroncal@monentia.es>
2019-01-25 08:34:42 +0000

    updated logger

--- alexandria/logger/src/io/intino/alexandria/logger/Formatter.java
+++ alexandria/logger/src/io/intino/alexandria/logger/Formatter.java
@@ -3,6 +3,13 @@ package io.intino.alexandria.logger;
 
 public class Formatter extends java.util.logging.Formatter {
 
+	public Formatter() {
+	}
+
+	@Deprecated
+	public Formatter(String type) {
+	}
+
 	@Override
 	public String format(java.util.logging.LogRecord record) {
 		return Logger.format(record);
