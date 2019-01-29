d678cff9c2991622951782f2edd2de88126aa177
Mario Caballero Ramírez <mariocaballero@siani.es>
2019-01-29 09:04:20 +0000

    no message

--- ide/builder/src/io/intino/konos/builder/codegeneration/accessor/ui/resource/ResourceRenderer.java
+++ ide/builder/src/io/intino/konos/builder/codegeneration/accessor/ui/resource/ResourceRenderer.java
@@ -2,11 +2,13 @@ package io.intino.konos.builder.codegeneration.accessor.ui.resource;
 
 import io.intino.konos.builder.codegeneration.Settings;
 import io.intino.konos.builder.codegeneration.accessor.ui.templates.PageTemplate;
+import io.intino.konos.model.graph.Display;
 import io.intino.konos.model.graph.KonosGraph;
 import io.intino.konos.model.graph.ui.UIService;
 import org.siani.itrules.model.Frame;
 
 import java.io.File;
+import java.util.UUID;
 
 import static io.intino.konos.builder.helpers.Commons.firstUpperCase;
 import static io.intino.konos.builder.helpers.Commons.write;
@@ -30,6 +32,7 @@ public class ResourceRenderer extends io.intino.konos.builder.codegeneration.ui.
 
 	private void writeJavascript(Frame frame) {
 		frame.addTypes("js");
+		frame.addSlot("id", UUID.randomUUID().toString());
 		File destiny = createIfNotExists(new File(accessorSrc() + File.separator + "pages" + File.separator));
 		write(new File(destiny + File.separator + firstUpperCase(resource.name$()) + ".js").toPath(), setup(PageTemplate.create()).format(frame));
 	}
@@ -38,8 +41,17 @@ public class ResourceRenderer extends io.intino.konos.builder.codegeneration.ui.
 	public Frame buildFrame() {
 		Frame frame = super.buildFrame().addTypes("resource");
 		frame.addSlot("name", resource.name$());
-		frame.addSlot("pageDisplay", KonosGraph.displayFor(resource).name$());
+		Display display = KonosGraph.displayFor(resource);
+		frame.addSlot("pageDisplay", display.name$());
+		frame.addSlot("pageDisplayType", typeOf(display));
+		addPageDisplayOrigin(frame, display);
 		return frame;
+	}
+
+	private void addPageDisplayOrigin(Frame frame, Display display) {
+		Frame originFrame = new Frame();
+		if (display.isDecorated()) originFrame.addSlot("src", "");
+		frame.addSlot("pageDisplayOrigin", originFrame);
 	}
 
 }
