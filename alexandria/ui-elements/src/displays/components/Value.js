7ddc042929181bca8a3bf0bee3bedafb49c15f30
Mario Caballero Ramírez <mariocaballero@siani.es>
2019-01-31 19:27:02 +0000

    alexandria-ui: adding PushService

--- alexandria/ui-elements/src/displays/components/Value.js
+++ alexandria/ui-elements/src/displays/components/Value.js
@@ -2,6 +2,15 @@ import React from "react";
 import AbstractValue from "../../../gen/displays/components/AbstractValue";
 
 export default class Value extends AbstractValue {
-	update = (value) => {
+	state = {
+		value : ""
 	};
+
+	update = (value) => {
+		this.setState({ "value": value });
+	};
+
+	render() {
+		<div>{this.state.value}</div>
+	}
 }
