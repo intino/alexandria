6be2811b2561d64e21cd3b609c4face509c28b1b
Mario Caballero Ramírez <mariocaballero@siani.es>
2019-02-01 18:28:33 +0000

    Adding new server side approach

--- ide/builder/src/io/intino/konos/builder/codegeneration/accessor/ui/templates/PassiveViewNotifierTemplate.java
+++ ide/builder/src/io/intino/konos/builder/codegeneration/accessor/ui/templates/PassiveViewNotifierTemplate.java
@@ -18,10 +18,13 @@ public class PassiveViewNotifierTemplate extends Template {
 
 	public Template define() {
 		add(
-			rule().add((condition("type", "display & accessible"))).add(literal("import Notifier from \"./Notifier\";\n\nexport default class ")).add(mark("name", "firstUpperCase")).add(literal("Notifier extends Notifier {\n\nconstructor(element) {\n    super(element);\n    this.setup();\n};\n\nsetup = () => {\n    this.pushService.listen((message) => {\n    \tif (message.target !== this.element.id) return;\n\t\tif (message.operation === \"refreshBaseUrl\")\n\t\t\tthis.refreshBaseUrl(message.parameters.value);\n\t\telse if (message.operation === \"refreshError\")\n\t\t\tthis.refreshError(message.parameters.value);\n    });\n};\n}")),
-			rule().add((condition("type", "display"))).add(literal("import Notifier from \"./Notifier\";\n\nexport default class ")).add(mark("name", "firstUpperCase")).add(literal("Notifier extends Notifier {\n\nconstructor(element) {\n    super(element);\n    this.setup();\n};\n\nsetup = () => {\n    this.pushService.listen((message) => {\n    \tif (message.target !== this.element.id) return;\n    \t")).add(mark("notification").multiple("\n")).add(literal("\n    });\n};\n}")),
-			rule().add((condition("trigger", "notification"))).add(literal("if (message.operation === \"")).add(mark("name")).add(literal("\")\n\tthis.")).add(mark("name")).add(literal("(")).add(expression().add(mark("parameter"))).add(literal(");")),
-			rule().add((condition("trigger", "parameter"))).add(literal("message.parameters.value"))
+			rule().add((condition("type", "display & accessible"))).add(literal("import Notifier from \"./Notifier\";\n\nexport default class ")).add(mark("name", "firstUpperCase")).add(literal("Notifier extends Notifier {\n\nconstructor(element) {\n    super(element);\n    this.setup();\n};\n\nsetup = () => {\n\tif (this.element == null || this.pushLinked) return;\n\tthis.when(\"refreshBaseUrl\").toSelf().execute((parameters) => this.refreshBaseUrl(parameters.value));\n\tthis.when(\"refreshError\").toSelf().execute((parameters) => this.refreshError(parameters.value));\n\tthis.pushLinked = true;\n};\n}")),
+			rule().add((condition("type", "display"))).add(literal("import Notifier from \"./Notifier\";\n\nexport default class ")).add(mark("name", "firstUpperCase")).add(literal("Notifier extends Notifier {\n\nconstructor(element) {\n    super(element);\n    this.setup();\n};\n\nsetup = () => {\n\tif (this.element == null || this.pushLinked) return;\n\t")).add(mark("notification").multiple("\n")).add(literal("\n\tthis.pushLinked = true;\n};\n}")),
+			rule().add((condition("trigger", "notification"))).add(literal("this.when(\"")).add(mark("name")).add(literal("\")")).add(expression().add(mark("target"))).add(literal(".execute((")).add(expression().add(mark("parameter", "call"))).add(literal(") => this.")).add(mark("name")).add(literal("(")).add(expression().add(mark("parameter", "value"))).add(literal("));")),
+			rule().add((condition("type", "parameter")), (condition("trigger", "call"))).add(literal("parameters")),
+			rule().add((condition("type", "parameter")), (condition("trigger", "value"))).add(literal("parameters.value")),
+			rule().add((condition("attribute", "Display")), (condition("trigger", "target"))).add(literal(".toSelf()")),
+			rule().add((condition("trigger", "target")))
 		);
 		return this;
 	}
