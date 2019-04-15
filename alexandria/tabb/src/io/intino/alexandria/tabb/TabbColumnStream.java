caa2b53f7d69898a70773912d252c272a406aa15
Octavio Roncal <octavioroncal@monentia.es>
2019-04-15 07:40:24 +0100

    updated tabb

---
+++ alexandria/tabb/src/io/intino/alexandria/tabb/TabbColumnStream.java
@@ -0,0 +1,77 @@
+package io.intino.alexandria.tabb;
+
+import io.intino.alexandria.logger.Logger;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+class TabbColumnStream implements ColumnStream {
+
+	private final File file;
+	private final TabbInfo.ColumnInfo column;
+	private final byte[] value;
+	private InputStream inputStream;
+
+	public TabbColumnStream(File file, TabbInfo.ColumnInfo column) {
+		this.file = file;
+		this.column = column;
+		value = new byte[size()];
+		try {
+			inputStream = ZipEntryReader.openEntry(file, column.name + ColumnStream.ColumnExtension);
+		} catch (IOException e) {
+			Logger.error(e);
+		}
+
+	}
+
+	private int size() {
+		if (column.type.equals(Type.Double)) return Double.SIZE / 8;
+		else if (column.type.equals(Type.Long)) return Long.SIZE / 8;
+		else return Integer.SIZE / 8;
+	}
+
+	@Override
+	public String name() {
+		return file.getName().replace(ColumnExtension, "");
+	}
+
+	@Override
+	public Type type() {
+		return column.type;
+	}
+
+	@Override
+	public Mode mode() {
+		return new Mode(column.modes);
+	}
+
+	@Override
+	public boolean hasNext() {
+		try {
+			return inputStream.available() > 0;
+		} catch (IOException e) {
+			Logger.error(e);
+			return false;
+		}
+	}
+
+	@Override
+	public void next() {
+		try {
+			inputStream.read(value);
+		} catch (IOException e) {
+			Logger.error(e);
+		}
+	}
+
+	@Override
+	public Long key() {
+		return null;
+	}
+
+	@Override
+	public byte[] value() {
+		return value;
+	}
+}
--- alexandria/tabb/src/io/intino/alexandria/tabb/TabbReader.java
+++ alexandria/tabb/src/io/intino/alexandria/tabb/TabbReader.java
@@ -1,15 +1,16 @@
 package io.intino.alexandria.tabb;
 
+import io.intino.alexandria.tabb.ColumnStream.NotAvailable;
 import io.intino.alexandria.tabb.ColumnStream.Type;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.ByteBuffer;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 
+import static java.nio.ByteBuffer.wrap;
 import static java.util.stream.Collectors.toList;
 
 public class TabbReader {
@@ -19,7 +20,7 @@ public class TabbReader {
 	public TabbReader(File file, String... columns) throws IOException {
 		this.info = TabbInfo.of(file);
 		this.columns = Arrays.stream(info.columns(columns))
-				.map(c -> ColumnStreamReader.of(file, c))
+				.map(c -> new TabbColumnStream(file, c))
 				.collect(toList());
 	}
 
@@ -45,7 +46,7 @@ public class TabbReader {
 		private final ColumnStream.Mode mode;
 		private final byte[] value;
 
-		public Value(Type type, ColumnStream.Mode mode, byte[] value) {
+		Value(Type type, ColumnStream.Mode mode, byte[] value) {
 			this.type = type;
 			this.mode = mode;
 			this.value = value;
@@ -60,23 +61,23 @@ public class TabbReader {
 		}
 
 		public boolean isAvailable() {
-			return Arrays.equals(ColumnStream.NotAvailable.bytesOf(type), value);
+			return !Arrays.equals(NotAvailable.bytesOf(type), value);
 		}
 
 		public int asInteger() {
-			return isAvailable() ? ByteBuffer.wrap(value).getInt() : ColumnStream.NotAvailable.NaInt;
+			return isAvailable() ? wrap(value).getInt() : NotAvailable.NaInt;
 		}
 
 		public double asDouble() {
-			return isAvailable() ? ByteBuffer.wrap(value).getDouble() : ColumnStream.NotAvailable.NaDouble;
+			return isAvailable() ? wrap(value).getDouble() : NotAvailable.NaDouble;
 		}
 
 		public boolean asBoolean() {
-			return ByteBuffer.wrap(value).getInt() == 1;//FIXME Na??
+			return wrap(value).getInt() == 1;//FIXME Na??
 		}
 
 		public Long asLong() {
-			return isAvailable() ? ByteBuffer.wrap(value).getLong() : ColumnStream.NotAvailable.NaDouble;
+			return isAvailable() ? wrap(value).getLong() : NotAvailable.NaDouble;
 		}
 
 		public LocalDateTime asDatetime() {
@@ -88,7 +89,7 @@ public class TabbReader {
 		}
 
 		public String asString() {
-			return mode.features[ByteBuffer.wrap(value).getInt()];
+			return mode.features[wrap(value).getInt()];
 		}
 
 	}
