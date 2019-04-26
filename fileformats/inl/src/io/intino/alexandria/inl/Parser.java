ad33e00a0e55eb77325ad7895a87da49f0e61e69
Octavio Roncal <octavioroncal@monentia.es>
2019-04-26 12:26:29 +0100

    refactor inl updated primitives and rest dependencies

--- fileformats/inl/src/io/intino/alexandria/inl/Parser.java
+++ fileformats/inl/src/io/intino/alexandria/inl/Parser.java
@@ -1,7 +1,6 @@
 package io.intino.alexandria.inl;
 
 interface Parser {
-    String NullValue = "\0";
     Object parse(String text);
 
     static Parser of(Class<?> aClass) { return ParserFactory.get(aClass); }
