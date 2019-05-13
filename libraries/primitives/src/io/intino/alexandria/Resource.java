e74293e50669da0041c2c2e68757e184bede0fdf
Octavio Roncal <octavioroncal@monentia.es>
2019-05-13 07:30:30 +0100

    updated resource -> updated rest and rest accessor
    pending inl

--- libraries/primitives/src/io/intino/alexandria/Resource.java
+++ libraries/primitives/src/io/intino/alexandria/Resource.java
@@ -1,56 +1,87 @@
 package io.intino.alexandria;
 
-import java.io.*;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.ByteBuffer;
-import java.util.Base64;
 
-import static java.net.URLConnection.getFileNameMap;
-import static java.net.URLConnection.guessContentTypeFromName;
-import static java.net.URLConnection.guessContentTypeFromStream;
+import static java.net.URLConnection.*;
 
 public class Resource {
 	private final String name;
 	private final String extension;
-	private byte[] data;
+	private final Metadata metadata;
+	private final InputStream inputStream;
 
-	public Resource(String name, byte[] data) {
-		this.name = name;
-		this.extension = name.substring(name.lastIndexOf('.') + 1);
-		this.data = data;
+	public Resource(String name, byte[] bytes) {
+		this(name, null, new ResourceInputStream(bytes));
 	}
 
-	public Resource(String name, InputStream data) throws IOException {
-		this(name, read(data));
+	public Resource(String name, String contentType, byte[] bytes) {
+		this(name, contentType, new ResourceInputStream(bytes));
 	}
 
-    public String name() {
-        return name;
-    }
+	public Resource(String name, InputStream inputStream) {
+		this(name, null, inputStream);
+	}
 
-    public String type() {
-        return extension;
-    }
+	public Resource(String name, String contentType, InputStream inputStream) {
+		this.name = complete(name, contentType);
+		this.extension = this.name.substring(name.lastIndexOf('.') + 1);
+		this.inputStream = inputStream;
+		this.metadata = new Metadata(contentType);
+	}
 
-    public byte[] data() {
-        return data;
-    }
+	private String complete(String name, String contentType) {
+		if (contentType == null) return name;
+		String extension = extensionOf(contentType);
+		return name + (name.endsWith(extension) ? "" : extension);
+	}
 
-    public InputStream stream() {
-        return new ByteArrayInputStream(data);
-    }
+	private String extensionOf(String contentType) {
+		if (contentType == null) return "";
+		throw new RuntimeException("TODO. No te olvides de implementarlo");
+		//return ".pdf";
+	}
 
-    private static byte[] read(InputStream is) throws IOException {
-        byte[] buffer = new byte[4096];
-        ByteBuffer bb = ByteBuffer.allocate(3);
-        while (is.available() > 0) {
-            int length = is.read(buffer);
-            bb.put(buffer,0,length);
-        }
-        return bb.array();
-    }
+	public String name() {
+		return name;
+	}
+
+	public String type() {
+		return extension;
+	}
+
+	public byte[] bytes() {
+		return inputStream instanceof ResourceInputStream ? resourceInputStream().bytes() : read(stream());
+	}
+
+	public InputStream stream() {
+		return inputStream instanceof ResourceInputStream ? resourceInputStream() : inputStream;
+	}
+
+	private ResourceInputStream resourceInputStream() {
+		ResourceInputStream stream = (ResourceInputStream) this.inputStream;
+		stream.reset();
+		return stream;
+	}
 
 	public Metadata metadata() {
-	    return new Metadata(this);
+		return metadata;
+	}
+
+	private static byte[] read(InputStream is) {
+		try {
+			byte[] buffer = new byte[4096];
+			ByteBuffer bb = ByteBuffer.allocate(0);
+			while (is.available() > 0) {
+				int length = is.read(buffer);
+				bb.put(buffer, 0, length);
+			}
+			return bb.array();
+		} catch (Exception ignored) {
+			return null;
+		}
     }
 
 	@Override
@@ -65,20 +96,13 @@ public class Resource {
         return new Resource(name, data);
     }
 
-    public void data(byte[] data) {
-        this.data = data;
-    }
+	public class Metadata {
+		private String mimeType;
+		private String contentType;
 
-    public static class Metadata {
-        private final Resource resource;
-        private final String contentType;
-        private final String mimeType;
-
-        Metadata(Resource resource) {
-            this.resource = resource;
-            this.contentType = guessContentTypeFromName(resource.name);
-            this.mimeType = getFileNameMap().getContentTypeFor(resource.name);
-
+		Metadata(String contentType) {
+			this.mimeType = getFileNameMap().getContentTypeFor(Resource.this.name);
+			this.contentType = contentType == null ? guessContentTypeFromName(Resource.this.name) : contentType;
         }
 
         public String mimeType() {
@@ -89,14 +113,30 @@ public class Resource {
             return contentType != null ? contentType : resolveContentType();
         }
 
+		public Resource contentType(String contentType) {
+			this.contentType = contentType;
+			return Resource.this;
+		}
+
         private String resolveContentType() {
             try {
-                return guessContentTypeFromStream(new ByteArrayInputStream(resource.data));
-            } catch (IOException e) {
+				return inputStream instanceof ByteArrayInputStream ? guessContentTypeFromStream(Resource.this.stream()) : null;
+			} catch (IOException e) {
                 return null;
             }
         }
 
 
     }
+
+	private static class ResourceInputStream extends ByteArrayInputStream {
+		ResourceInputStream(byte[] buf) {
+			super(buf);
+		}
+
+		byte[] bytes() {
+			return buf;
+		}
+	}
+
 }
