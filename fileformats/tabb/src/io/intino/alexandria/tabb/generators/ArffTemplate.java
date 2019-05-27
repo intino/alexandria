11185ee8271c1a1d8e2b8de02a1c09d910683b00
Octavio Roncal <octavioroncal@monentia.es>
2019-05-27 09:48:08 +0100

    updated arff generation in tabb

--- fileformats/tabb/src/io/intino/alexandria/tabb/generators/ArffTemplate.java
+++ fileformats/tabb/src/io/intino/alexandria/tabb/generators/ArffTemplate.java
@@ -12,7 +12,8 @@ public class ArffTemplate extends Template {
 				rule().condition((type("nominal")), (trigger("type"))).output(literal("{")).output(mark("value", "quoted").multiple(",")).output(literal("}")),
 				rule().condition((type("date")), (trigger("type"))).output(literal("DATE \"")).output(mark("format")).output(literal("\"")),
 				rule().condition((type("numeric")), (trigger("type"))).output(literal("NUMERIC")),
-				rule().condition((type("string")), (trigger("type"))).output(literal("string"))
+				rule().condition((type("string")), (trigger("type"))).output(literal("string")),
+				rule().condition((trigger("quoted"))).output(literal("\"")).output(mark("")).output(literal("\""))
 		);
 	}
 }
