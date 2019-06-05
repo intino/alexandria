0591eacc5ad5e1ea90b32459a511fdff73e8eb1a
Octavio Roncal <octavioroncal@monentia.es>
2019-06-05 10:15:37 +0100

    updated mappcolumnStreamer

--- fileformats/tabb/src/io/intino/alexandria/tabb/streamers/MappColumnStreamer.java
+++ fileformats/tabb/src/io/intino/alexandria/tabb/streamers/MappColumnStreamer.java
@@ -1,61 +1,114 @@
 package io.intino.alexandria.tabb.streamers;
 
-import io.intino.alexandria.Timetag;
 import io.intino.alexandria.mapp.MappReader;
 import io.intino.alexandria.mapp.MappStream;
 import io.intino.alexandria.tabb.ColumnStream;
 import io.intino.alexandria.tabb.ColumnStream.Type;
 import io.intino.alexandria.tabb.ColumnStreamer;
-import io.intino.alexandria.tabb.Function;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
 
 public class MappColumnStreamer implements ColumnStreamer {
 	private final MappReader reader;
-	private final Type type;
-	private final Timetag timetag;
-	private final Function<String, ?> function;
-	private final String name;
+	private List<Selector> selectors = new ArrayList<>();
 
-	public MappColumnStreamer(MappReader reader, String name, Type type, Timetag timetag, Function<String, ?> function) {
+	private MappStream.Item current = nullItem();
+
+	public MappColumnStreamer(MappReader reader) {
 		this.reader = reader;
-		this.name = name;
-		this.type = type;
-		this.timetag = timetag;
-		this.function = function;
+	}
+
+	private static MappStream.Item nullItem() {
+		return new MappStream.Item() {
+			@Override
+			public long key() {
+				return -1;
+			}
+
+			@Override
+			public String value() {
+				return null;
+			}
+		};
+	}
+
+	public void add(Selector selector) {
+		selectors.add(selector);
 	}
 
 	@Override
 	public ColumnStream[] get() {
-		return new ColumnStream[]{create()};
+		return selectors.stream().map(s -> new ColumnStream() {
+			private MappStream.Item current = nullItem();
+
+			@Override
+			public String name() {
+				return s.name();
+			}
+
+			@Override
+			public Type type() {
+				return s.type();
+			}
+
+			@Override
+			public boolean hasNext() {
+				return getNext(current.key());
+			}
+
+			@Override
+			public void next() {
+				current = getCurrent(current.key() + 1);
+			}
+
+			@Override
+			public Long key() {
+				return current.key();
+			}
+
+			@Override
+			public Object value() {
+				return s.select(current);
+			}
+		}).toArray(ColumnStream[]::new);
 	}
 
-	private ColumnStream create() {
-		return new MappColumnStream(reader, name, type, timetag, function);
+	private boolean getNext(long key) {
+		return key < this.current.key() || reader.hasNext();
 	}
 
-	public class MappColumnStream implements ColumnStream {
-		private final MappReader reader;
-		private final String name;
-		private final Type type;
-		private final Timetag timetag;
-		private final Function<String, ?> function;
-		private final Map<String, Integer> labels;
-		private MappStream.Item current;
+	private MappStream.Item getCurrent(long key) {
+		if (key > this.current.key()) this.current = reader.next();
+		return this.current;
+	}
 
-		public MappColumnStream(MappReader reader, String name, Type type, Timetag timetag, Function<String, ?> function) {
-			this.reader = reader;
-			this.type = type;
+	public interface Selector {
+
+		String name();
+
+		Type type();
+
+		Object select(MappStream.Item item);
+
+	}
+
+	public static class SimpleSelector implements Selector {
+
+		private String name;
+		private Type type;
+		private Function<MappStream.Item, ?> function;
+
+		public SimpleSelector(String name, Type type, Function<MappStream.Item, ?> function) {
 			this.name = name;
-			this.timetag = timetag;
+			this.type = type;
 			this.function = function;
-			this.labels = new HashMap<>();
 		}
 
 		@Override
 		public String name() {
-			return name == null ? reader.name() : name;
+			return name;
 		}
 
 		@Override
@@ -64,38 +117,19 @@ public class MappColumnStreamer implements ColumnStreamer {
 		}
 
 		@Override
-		public Mode mode() {
-			return new Mode(labels.keySet().stream()
-					.map(l -> l.replace("\n", "|"))
-					.toArray(String[]::new));
+		public Object select(MappStream.Item item) {
+			return function.apply(item);
 		}
 
-		@Override
-		public boolean hasNext() {
-			return reader.hasNext();
-		}
+	}
 
-		@Override
-		public void next() {
-			current = reader.next();
-		}
+	public static class BypassSelector extends SimpleSelector {
 
-		@Override
-		public Long key() {
-			return current != null ? current.key() : null;
-		}
-
-		@Override
-		public Object value() {
-			if (type == Type.Nominal) return factorize(function.apply(current.key(), current.value(), timetag));
-			return current.value();
-		}
-
-		private int factorize(Object object) {
-			String value = object.toString();
-			if (!labels.containsKey(value)) labels.put(value, labels.size());
-			return labels.get(value);
+		public BypassSelector(String name, Type type) {
+			super(name, type, MappStream.Item::value);
 		}
 
 	}
 }
+
+
