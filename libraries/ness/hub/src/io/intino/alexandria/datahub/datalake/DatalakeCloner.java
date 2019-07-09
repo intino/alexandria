c9eceae75822c1481130acd41b7cbdbbc93a2828
Octavio Roncal <octavioroncal@monentia.es>
2019-07-09 12:03:02 +0100

    updated imls. updated hub with tested mirroring

--- libraries/ness/hub/src/io/intino/alexandria/datahub/datalake/DatalakeCloner.java
+++ libraries/ness/hub/src/io/intino/alexandria/datahub/datalake/DatalakeCloner.java
@@ -1,71 +1,94 @@
 package io.intino.alexandria.datahub.datalake;
 
-import com.jcraft.jsch.*;
 import io.intino.alexandria.datahub.model.Configuration;
 import io.intino.alexandria.logger.Logger;
+import net.schmizz.sshj.SSHClient;
+import net.schmizz.sshj.sftp.RemoteResourceInfo;
+import net.schmizz.sshj.sftp.SFTPClient;
+import net.schmizz.sshj.sftp.SFTPFileTransfer;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
-import java.util.Vector;
 import java.util.stream.Collectors;
 
 import static io.intino.alexandria.datahub.model.Configuration.Tanks.Tank.Type.Event;
 import static io.intino.alexandria.datahub.model.Configuration.Tanks.Tank.Type.Set;
+import static java.io.File.separator;
 
 
 public class DatalakeCloner {
 	private final Configuration.Tanks tanks;
-	private final Configuration.Mirror mirror;
+	private final Configuration.Mirror conf;
 
-	public DatalakeCloner(Configuration.Mirror mirror, Configuration.Tanks tanks) {
-		this.mirror = mirror;
+	public DatalakeCloner(Configuration.Mirror conf, Configuration.Tanks tanks) {
+		this.conf = conf;
 		this.tanks = tanks;
 	}
 
 	public void execute() {
-		Logger.info("Cloning datalake from " + mirror.originUrl() + " to " + mirror.datalakeDestinationPath());
-		JSch jsch = new JSch();
-		String[] split = mirror.originUrl().split(":");
+		Logger.info("Mirroring datalake from " + conf.originUrl() + " to " + conf.datalakeDestinationPath());
+		Logger.includeLevel(Logger.Level.TRACE);
+		SSHClient ssh = new SSHClient();
+		String[] urlAndPort = conf.originUrl().split(":");
 		try {
-			Session session = jsch.getSession(mirror.user(), mirror.originUrl().split(":")[0], split.length == 2 ? Integer.parseInt(split[1]) : 22);
-			session.connect();
-			Channel channel = session.openChannel("sftp");
-			channel.connect();
-			ChannelSftp c = (ChannelSftp) channel;
-			cloneEventTanks(c);
-			cloneSetTanks(c);
-		} catch (JSchException e) {
+			ssh.loadKnownHosts();
+			ssh.connect(urlAndPort[0], Integer.parseInt(urlAndPort[1]));
+			ssh.authPublickey("root", System.getProperty("user.home") + separator + ".ssh" + separator + "id_rsa");
+			SFTPClient sftpClient = ssh.newSFTPClient();
+			cloneEventTanks(sftpClient);
+			cloneSetTanks(sftpClient);
+		} catch (IOException e) {
 			Logger.error(e);
+		}
+		Logger.info("Mirroring finished");
+	}
+
+	private void cloneEventTanks(SFTPClient client) throws IOException {
+		SFTPFileTransfer fileTransfer = client.getFileTransfer();
+		for (Configuration.Tanks.Tank tank : eventTanks()) {
+			File destinationTank = new File(conf.datalakeDestinationPath() + separator + "events" + separator + tank.name());
+			destinationTank.mkdirs();
+			List<RemoteResourceInfo> zims = client.ls(conf.datalakeOriginPath() + "/events/" + tank.name());
+			zims.sort(Comparator.comparing(RemoteResourceInfo::getName));
+			downloadZims(fileTransfer, zims, destinationTank);
 		}
 	}
 
-	private void cloneEventTanks(ChannelSftp c) {
-		String startingTimetag = mirror.startingTimetag() + ".zim";
-		tanks.tanks().stream().filter(t -> t.type().equals(Event)).forEach(tank -> {
-			try {
-				c.lcd(mirror.datalakeDestinationPath());
-				c.cd(mirror.datalakeOriginPath() + "/events/" + tank.name());
-				Vector<ChannelSftp.LsEntry> list = c.ls(".");
-				for (ChannelSftp.LsEntry item : list)
-					if (item.getFilename().compareTo(startingTimetag) >= 0) c.get(item.getFilename(), item.getFilename());
-			} catch (SftpException e) {
-				Logger.error(e);
-			}
-		});
+	private void downloadZims(SFTPFileTransfer fileTransfer, List<RemoteResourceInfo> zims, File destinationTank) throws IOException {
+		String startingTimetag = conf.startingTimetag() + ".zim";
+		for (RemoteResourceInfo zim : zims)
+			if (zim.getName().replace(".zim", "").compareTo(startingTimetag) >= 0 && !new File(destinationTank, zim.getName()).exists())
+				fileTransfer.download(zim.getPath(), new File(destinationTank, zim.getName()).getPath());
 	}
 
-	private void cloneSetTanks(ChannelSftp c) {
-		String startingTimetag = mirror.startingTimetag();
-		tanks.tanks().stream().filter(t -> t.type().equals(Set)).map(this::namesOf).flatMap(Collection::stream).forEach(tank -> {
-			try {
-				c.lcd(mirror.datalakeDestinationPath());
-				c.cd(mirror.datalakeOriginPath() + "/sets/" + tank);
-				cloneSetTank(c, startingTimetag);
-			} catch (SftpException e) {
-				Logger.error(e);
-			}
-		});
+	private void cloneSetTanks(SFTPClient client) throws IOException {
+		SFTPFileTransfer fileTransfer = client.getFileTransfer();
+		for (String tank : setTanks()) {
+			File destinationTank = new File(conf.datalakeDestinationPath() + separator + "sets" + separator + tank + separator);
+			destinationTank.mkdirs();
+			List<RemoteResourceInfo> tubs = client.ls(conf.datalakeOriginPath() + "/sets/" + tank);
+			tubs.sort(Comparator.comparing(RemoteResourceInfo::getName));
+			downloadZets(fileTransfer, destinationTank, tubs);
+		}
+	}
+
+	private void downloadZets(SFTPFileTransfer fileTransfer, File destinationTank, List<RemoteResourceInfo> tubs) throws IOException {
+		String startingTimetag = conf.startingTimetag();
+		for (RemoteResourceInfo tub : tubs)
+			if (tub.getName().compareTo(startingTimetag) >= 0 && !new File(destinationTank, tub.getName()).exists())
+				fileTransfer.download(tub.getPath(), new File(destinationTank, tub.getName()).getPath());
+	}
+
+	private List<Configuration.Tanks.Tank> eventTanks() {
+		return tanks.tanks().stream().filter(t -> t.type().equals(Event)).collect(Collectors.toList());
+	}
+
+	private List<String> setTanks() {
+		return tanks.tanks().stream().filter(t -> t.type().equals(Set)).map(this::namesOf).flatMap(Collection::stream).collect(Collectors.toList());
 	}
 
 	private List<String> namesOf(Configuration.Tanks.Tank tank) {
@@ -73,12 +96,5 @@ public class DatalakeCloner {
 		Configuration.Tanks.Split split = tanks.splits().stream().filter(s -> s.name().equals(tank.split())).findFirst().orElse(null);
 		if (split == null) return Collections.singletonList(tank.name());
 		return split.values().stream().map(v -> v + "." + tank.name()).collect(Collectors.toList());
-	}
-
-	private void cloneSetTank(ChannelSftp c, String startingTimetag) throws SftpException {
-		Vector<ChannelSftp.LsEntry> list = c.ls(".");
-		for (ChannelSftp.LsEntry item : list)
-			if (item.getAttrs().isDir() && item.getFilename().compareTo(startingTimetag) >= 0)
-				c.get(item.getFilename(), item.getFilename());
 	}
 }
