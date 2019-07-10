30e0c0d937c56c8f6421331067d42234972d4040
Jose Évora <jose.evora@siani.es>
2019-07-10 07:15:36 +0100

    updated bpm

--- framework/bpm/src/io/intino/alexandria/bpm/State.java
+++ framework/bpm/src/io/intino/alexandria/bpm/State.java
@@ -1,18 +1,25 @@
 package io.intino.alexandria.bpm;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import static io.intino.alexandria.bpm.State.Type.Initial;
+import static io.intino.alexandria.bpm.State.Type.Terminal;
+import static java.util.Arrays.asList;
+
 public class State {
 	private final String name;
 	private final Task task;
-	private final Type type;
+	private final Set<Type> type;
 
 	public State(String name, Task task) {
 		this(name, task, Type.Normal);
 	}
 
-	public State(String name, Task task, Type type) {
+	public State(String name, Task task, Type... type) {
 		this.name = name;
 		this.task = task;
-		this.type = type;
+		this.type = new HashSet<>(asList(type));
 	}
 
 	public String name() {
@@ -23,8 +30,12 @@ public class State {
 		return task;
 	}
 
-	public Type type() {
-		return type;
+	public boolean isInitial(){
+		return type.contains(Initial);
+	}
+
+	public boolean isTerminal() {
+		return type.contains(Terminal);
 	}
 
 	public enum Type {Initial, Normal, Terminal}
