fde06a24d3d13e3e5329e3c0ccb590c4e9aa66c0
Octavio Roncal <octavioroncal@monentia.es>
2019-07-12 10:03:07 +0100

    updated datalake ingestion and sealing

--- libraries/ness/ingestion/src/io/intino/alexandria/ingestion/FS.java
+++ libraries/ness/ingestion/src/io/intino/alexandria/ingestion/FS.java
@@ -6,7 +6,6 @@ import java.io.File;
 import java.io.FileFilter;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
@@ -18,7 +17,7 @@ import java.util.stream.Stream;
 public class FS {
 	private File root;
 
-	public FS(File root) {
+	private FS(File root) {
 		this.root = root;
 	}
 
@@ -34,6 +33,10 @@ public class FS {
 		return Arrays.stream(new FS(folder).foldersIn(filter, Sort.Normal));
 	}
 
+	public static Stream<File> allFilesIn(File directory, FileFilter filter) {
+		return allFilesIn(directory.toPath(), filter).stream();
+	}
+
 	private File[] foldersIn(FileFilter filter, Sort sort) {
 		File[] files = root.listFiles(filter);
 		files = files == null ? new File[0] : files;
@@ -42,7 +45,7 @@ public class FS {
 	}
 
 
-	public static void copyInto(File destination, InputStream inputStream) {
+	static void copyInto(File destination, InputStream inputStream) {
 		try {
 			Files.copy(inputStream, destination.toPath());
 			inputStream.close();
@@ -51,22 +54,15 @@ public class FS {
 		}
 	}
 
-	public static Stream<File> allFilesIn(File directory, FileFilter filter) {
-		return allFilesIn(directory.toPath(), filter).stream();
-	}
-
 	private static List<File> allFilesIn(Path path, FileFilter filter) {
 		List<File> files = new ArrayList<>();
-		try (DirectoryStream<Path> stream = Files.newDirectoryStream(path)) {
-			for (Path entry : stream)
-				if (Files.isDirectory(entry) && filter.accept(entry.toFile())) files.addAll(allFilesIn(entry, filter));
-				else if (filter.accept(entry.toFile())) files.add(entry.toFile());
+		try (Stream<Path> paths = Files.walk(path)) {
+			paths.filter(p -> Files.isRegularFile(p) && filter.accept(p.toFile())).forEach(p -> files.add(p.toFile()));
 		} catch (IOException e) {
 			Logger.error(e);
 		}
 		return files;
 	}
-
 
 	public enum Sort {
 		Normal((x, y) -> x.getName().compareTo(y.getName())),
