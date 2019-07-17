d3a1135030454b961afc826ca90b5f1acefc3a52
jevora <jose.evora@siani.es>
2019-07-17 10:05:10 +0100

    added assertions to all tests

--- framework/bpm/test/io/intino/alexandria/bpm/MessageHub_.java
+++ framework/bpm/test/io/intino/alexandria/bpm/MessageHub_.java
@@ -1,20 +1,33 @@
 package io.intino.alexandria.bpm;
 
 import io.intino.alexandria.message.Message;
+import io.intino.alexandria.message.MessageHub;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 class MessageHub_ implements MessageHub {
-	private List<OnMessageReceived> listeners = new ArrayList<>();
+	private List<Consumer<Message>> listeners = new ArrayList<>();
 
 	@Override
 	public void sendMessage(String channel, Message message) {
-		new Thread(() -> listeners.forEach(l -> l.process(message))).start();
+		new Thread(() -> listeners.forEach(l -> l.accept(message))).start();
 	}
 
 	@Override
-	public void registerListener(String channel, OnMessageReceived onMessageReceived) {
+	public void attachListener(String channel, Consumer<Message> onMessageReceived) {
 		listeners.add(onMessageReceived);
 	}
+
+	@Override
+	public void attachListener(String channel, String subscriberId, Consumer<Message> onMessageReceived) {
+
+	}
+
+	@Override
+	public void detachListeners(String channel) {
+
+	}
+
 }
