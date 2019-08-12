b93fee2dda066e5b5c05653848c5b74f7980232f
Mario Caballero Ramírez <mariocaballero@siani.es>
2019-08-12 12:25:36 +0100

    Added add method to context

--- framework/core/src/io/intino/alexandria/core/Context.java
+++ framework/core/src/io/intino/alexandria/core/Context.java
@@ -1,7 +1,14 @@
 package io.intino.alexandria.core;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Context extends HashMap<String, String> {
+	private Map<String, String> userMap = new HashMap<>();
+
+	public Context add(String name, String value) {
+		userMap.put(name, value);
+		return this;
+	}
 
 }
