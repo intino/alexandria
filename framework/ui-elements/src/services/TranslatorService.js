a2446e5da823c5bd2670b8685da56fa0e3af3303
Mario Caballero Ramírez <mariocaballero@siani.es>
2019-10-02 19:49:33 +0100

    alexandria-ui: fix bug loading application level translations. Added selectable style to header of table component.

--- framework/ui-elements/src/services/TranslatorService.js
+++ framework/ui-elements/src/services/TranslatorService.js
@@ -1,4 +1,5 @@
 import I18n from "../../gen/I18n";
+import AppI18n from "app-elements/gen/I18n";
 
 const TranslatorService = {
     create : function (configuration) {
@@ -6,8 +7,13 @@ const TranslatorService = {
         let translator = {};
 
         translator.translate = (word, params) => {
-            const i18n = I18n.load(language);
-            return translator.translateParams(i18n != null && i18n[word] != null ? i18n[word] : word, params);
+            const appi18n = AppI18n.load(language);
+            let wordTranslation = appi18n != null && appi18n[word] != null ? appi18n[word] : null;
+            if (wordTranslation == null) {
+                const i18n = I18n.load(language);
+                wordTranslation = i18n != null && i18n[word] != null ? i18n[word] : word;
+            }
+            return translator.translateParams(wordTranslation, params);
         };
 
         translator.translateParams = (text, params) => {
