812de2c93093a68595d19fed87e5ccf07b784765
Octavio Roncal <octavioroncal@monentia.es>
2019-11-18 11:10:50 +0000

    added methods to message

--- messaging/message/src/io/intino/alexandria/message/Message.java
+++ messaging/message/src/io/intino/alexandria/message/Message.java
@@ -5,6 +5,7 @@ import io.intino.alexandria.Resource;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.time.Instant;
 import java.util.*;
 
 import static io.intino.alexandria.message.Event.TS;
@@ -12,19 +13,30 @@ import static java.util.Arrays.stream;
 
 public class Message {
 	static final String AttachmentHeader = "[&]";
+	private static Value NullValue = new Value() {
+		@Override
+		public String data() {
+			return null;
+		}
+
+		@Override
+		public <T> T as(Class<T> type) {
+			return null;
+		}
+	};
 	private String type;
 	private Message owner;
 	private Event event;
 	private Map<String, Attribute> attributes;
-    private List<Message> components;
-    private Map<String, byte[]> attachments;
+	private List<Message> components;
+	private Map<String, byte[]> attachments;
 
-    public Message(String type) {
+	public Message(String type) {
 		this.type = type;
 		this.owner = null;
 		this.attributes = new LinkedHashMap<>();
-        this.attachments = new HashMap<>();
-        this.components = null;
+		this.attachments = new HashMap<>();
+		this.components = null;
 	}
 
 	public String type() {
@@ -40,8 +52,8 @@ public class Message {
 	}
 
 	public List<String> attributes() {
-        return new ArrayList<>(attributes.keySet());
-    }
+		return new ArrayList<>(attributes.keySet());
+	}
 
 	public Value get(final String attribute) {
 		return contains(attribute) ? new Value() {
@@ -91,6 +103,14 @@ public class Message {
 		return set(attribute, value.toString());
 	}
 
+	public Message set(String attribute, Instant value) {
+		return set(attribute, value.toString());
+	}
+
+	public Message set(String attribute, Long value) {
+		return set(attribute, value.toString());
+	}
+
 	public Message set(String attribute, Integer value) {
 		return set(attribute, value.toString());
 	}
@@ -104,10 +124,10 @@ public class Message {
 	}
 
 	public Message set(String attribute, String value, byte[] content) {
-        return set(attribute, value + "@" + attach(content));
+		return set(attribute, value + "@" + attach(content));
 	}
 
-    public Message append(String attribute, Boolean value) {
+	public Message append(String attribute, Boolean value) {
 		return append(use(attribute), value.toString());
 	}
 
@@ -126,42 +146,24 @@ public class Message {
 	private Message append(Attribute attribute, String value) {
 		attribute.value =
 				(attribute.value == null ? "" : attribute.value + "\n") +
-				(value == null ? "\0" : value);
+						(value == null ? "\0" : value);
 		return this;
 	}
 
-    public Message append(String attribute, String name, InputStream is) {
-        return append(attribute, name, contentOf(is));
-    }
+	public Message append(String attribute, String name, InputStream is) {
+		return append(attribute, name, contentOf(is));
+	}
 
-    public Message append(String attribute, String name, byte[] content) {
-        return append(attribute, name + "@" + attach(content));
-    }
+	public Message append(String attribute, String name, byte[] content) {
+		return append(attribute, name + "@" + attach(content));
+	}
 
-    public boolean isEvent() {
+	public boolean isEvent() {
 		return contains(TS);
 	}
 
 	public Event asEvent() {
 		return isEvent() ? event() : null;
-	}
-
-	private static String indent(String text) {
-		return "\n\t" + text.replaceAll("\\n", "\n\t");
-	}
-
-	private static byte[] contentOf(InputStream is) {
-		try (ByteArrayOutputStream os = new ByteArrayOutputStream()) {
-			int length;
-			byte[] data = new byte[16384];
-			while ((length = is.read(data, 0, data.length)) != -1)
-				os.write(data, 0, length);
-			os.flush();
-			return os.toByteArray();
-
-		} catch (IOException e) {
-			return new byte[0];
-		}
 	}
 
 	private Event event() {
@@ -176,12 +178,12 @@ public class Message {
 	}
 
 	public Message remove(String attribute) {
-	    detach(use(attribute).value);
+		detach(use(attribute).value);
 		attributes.remove(attribute.toLowerCase());
 		return this;
 	}
 
-    private Attribute use(String attribute) {
+	private Attribute use(String attribute) {
 		if (!contains(attribute)) add(new Attribute(attribute));
 		return attributes.get(attribute.toLowerCase());
 	}
@@ -190,31 +192,31 @@ public class Message {
 		attributes.put(attribute.name.toLowerCase(), attribute);
 	}
 
-    private String attach(byte[] content) {
-        String uuid = UUID.random();
-        attachments.put(uuid, content);
-        return uuid;
-    }
+	private String attach(byte[] content) {
+		String uuid = UUID.random();
+		attachments.put(uuid, content);
+		return uuid;
+	}
 
-    private void detach(String value) {
-        if ((value == null) || (!value.contains("@"))) return;
-        removeAttachments(value);
-    }
+	private void detach(String value) {
+		if ((value == null) || (!value.contains("@"))) return;
+		removeAttachments(value);
+	}
 
-    private void removeAttachments(String value) {
-        for (String item : value.split("\n"))
-            removeAttachment(uuidIn(item));
-    }
+	private void removeAttachments(String value) {
+		for (String item : value.split("\n"))
+			removeAttachment(uuidIn(item));
+	}
 
-    private String uuidIn(String item) {
-        return item.substring(item.indexOf('@') + 1);
-    }
+	private String uuidIn(String item) {
+		return item.substring(item.indexOf('@') + 1);
+	}
 
-    private void removeAttachment(String uuid) {
-        if (UUID.is(uuid)) attachments.remove(uuid);
-    }
+	private void removeAttachment(String uuid) {
+		if (UUID.is(uuid)) attachments.remove(uuid);
+	}
 
-    public List<Message> components() {
+	public List<Message> components() {
 		return components == null ? new ArrayList<>() : new ArrayList<>(components);
 	}
 
@@ -229,9 +231,9 @@ public class Message {
 	public void add(Message component) {
 		if (components == null) components = new ArrayList<>();
 		components.add(component);
-        component.owner = this;
-        component.attachments = attachments;
-    }
+		component.owner = this;
+		component.attachments = attachments;
+	}
 
 	public void add(List<Message> components) {
 		if (components == null) return;
@@ -256,15 +258,15 @@ public class Message {
 		return sb.toString();
 	}
 
-    boolean hasAttachments() {
-        return (owner == null) && (attachments.size() > 0);
-    }
+	boolean hasAttachments() {
+		return (owner == null) && (attachments.size() > 0);
+	}
 
-    Map<String, byte[]> allAttachments() {
-        return attachments;
-    }
+	Map<String, byte[]> allAttachments() {
+		return attachments;
+	}
 
-    private String stringOf(Attribute attribute) {
+	private String stringOf(Attribute attribute) {
 		return attribute.name + ":" + (isMultiline(attribute.value) ? indent(attribute.value) : " " + attribute.value);
 	}
 
@@ -284,59 +286,66 @@ public class Message {
 		return attributes.containsKey(attribute.toLowerCase());
 	}
 
-    public List<String> attachments() {
-        return new ArrayList<>(attachments.keySet());
-    }
-
-    public byte[] attachment(String id) {
-        return attachments.getOrDefault(id, new byte[0]);
-    }
-
-    Message put(Map<String, byte[]> attachments) {
-	    this.attachments = attachments;
-        return this;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        if (this == object) return true;
-        if (object == null || getClass() != object.getClass()) return false;
-        Message message = (Message) object;
-        return Objects.equals(type, message.type) &&
-               attributes.keySet().stream().allMatch(k-> attributeEquals(message, k)) &&
-               attachments.keySet().stream().allMatch(k-> attachmentEquals(message, k)) &&
-               Objects.equals(components, message.components) ;
-    }
-
-    private boolean attributeEquals(Message message, String key) {
-        return message.get(key).data().equals(get(key).data());
-    }
-
-    private boolean attachmentEquals(Message message, String key) {
-        return Arrays.equals(message.attachments.get(key), message.attachments.get(key));
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(type, owner, event, attributes, components, attachments);
-    }
-
-    public interface Value {
-        String data();
-		<T> T as(Class<T> type);
+	public List<String> attachments() {
+		return new ArrayList<>(attachments.keySet());
 	}
 
-	private static Value NullValue = new Value() {
-        @Override
-        public String data() {
-            return null;
-        }
+	public byte[] attachment(String id) {
+		return attachments.getOrDefault(id, new byte[0]);
+	}
 
-        @Override
-        public <T> T as(Class<T> type) {
-            return null;
-        }
-    };
+	Message put(Map<String, byte[]> attachments) {
+		this.attachments = attachments;
+		return this;
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		if (this == object) return true;
+		if (object == null || getClass() != object.getClass()) return false;
+		Message message = (Message) object;
+		return Objects.equals(type, message.type) &&
+				attributes.keySet().stream().allMatch(k -> attributeEquals(message, k)) &&
+				attachments.keySet().stream().allMatch(k -> attachmentEquals(message, k)) &&
+				Objects.equals(components, message.components);
+	}
+
+	private boolean attributeEquals(Message message, String key) {
+		return message.get(key).data().equals(get(key).data());
+	}
+
+	private boolean attachmentEquals(Message message, String key) {
+		return Arrays.equals(message.attachments.get(key), message.attachments.get(key));
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(type, owner, event, attributes, components, attachments);
+	}
+
+	private static String indent(String text) {
+		return "\n\t" + text.replaceAll("\\n", "\n\t");
+	}
+
+	private static byte[] contentOf(InputStream is) {
+		try (ByteArrayOutputStream os = new ByteArrayOutputStream()) {
+			int length;
+			byte[] data = new byte[16384];
+			while ((length = is.read(data, 0, data.length)) != -1)
+				os.write(data, 0, length);
+			os.flush();
+			return os.toByteArray();
+
+		} catch (IOException e) {
+			return new byte[0];
+		}
+	}
+
+	public interface Value {
+		String data();
+
+		<T> T as(Class<T> type);
+	}
 
 	static class Attribute {
 		String name;
@@ -354,19 +363,19 @@ public class Message {
 	}
 
 	private static class UUID {
-        private static int[] HyphenPositions = new int[] {8, 13, 18, 23};
+		private static int[] HyphenPositions = new int[]{8, 13, 18, 23};
 
-        static boolean is(String str) {
-            for (int n : HyphenPositions)
-                if (str.charAt(n) != '-') return false;
-            return true;
-        }
+		static boolean is(String str) {
+			for (int n : HyphenPositions)
+				if (str.charAt(n) != '-') return false;
+			return true;
+		}
 
 
-        static String random() {
-            return java.util.UUID.randomUUID().toString();
-        }
-    }
+		static String random() {
+			return java.util.UUID.randomUUID().toString();
+		}
+	}
 
 }
 
