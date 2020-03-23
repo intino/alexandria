2813338ca5be703d5f6b23b8426910f5eccaa032
Octavio Roncal <octavioroncal@monentia.es>
2020-03-23 23:07:29 +0000

    updated builder with rest and soap separated libraries with http and rest/soap

--- libraries/wss/src/io/intino/alexandria/wss/pushservice/AdapterProxy.java
+++
@@ -1,6 +0,0 @@
-package io.intino.alexandria.wss.pushservice;
-
-public interface AdapterProxy {
-    RequestAdapter requestAdapterOf(String name, Class clazz);
-    ResponseAdapter responseAdapterOf(String name);
-}
