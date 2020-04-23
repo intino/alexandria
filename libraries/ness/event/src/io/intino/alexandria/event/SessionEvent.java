e3ac3ea704b19d3bc562be5dc1446ba52a61afcc
Octavio Roncal <octavioroncal@monentia.es>
2020-04-23 12:26:39 +0100

    updated SessionEvent

--- libraries/ness/event/src/io/intino/alexandria/event/SessionEvent.java
+++ libraries/ness/event/src/io/intino/alexandria/event/SessionEvent.java
@@ -3,7 +3,9 @@ package io.intino.alexandria.event;
 import io.intino.alexandria.logger.Logger;
 import io.intino.alexandria.message.Message;
 
+import java.io.InputStream;
 import java.time.Instant;
+import java.util.List;
 import java.util.UUID;
 
 public class SessionEvent extends Event {
@@ -18,6 +20,70 @@ public class SessionEvent extends Event {
 		super(PATH);
 		ts = Instant.now();
 		ss = UUID.randomUUID() + "@" + ts.toString();
+	}
+
+	public Message.Value get(String attribute) {
+		return message.get(attribute);
+	}
+
+	public SessionEvent set(String attribute, Boolean value) {
+		message.set(attribute, value);
+		return this;
+	}
+
+	public SessionEvent set(String attribute, Instant value) {
+		message.set(attribute, value);
+		return this;
+	}
+
+	public SessionEvent set(String attribute, Long value) {
+		message.set(attribute, value);
+		return this;
+	}
+
+	public SessionEvent set(String attribute, Integer value) {
+		message.set(attribute, value);
+		return this;
+	}
+
+	public SessionEvent set(String attribute, Double value) {
+		message.set(attribute, value);
+		return this;
+	}
+
+	public SessionEvent set(String attribute, String value, InputStream is) {
+		message.set(attribute, value, is);
+		return this;
+	}
+
+	public SessionEvent set(String attribute, String value, byte[] content) {
+		message.set(attribute, value, content);
+		return this;
+	}
+
+	public SessionEvent set(String attribute, String value) {
+		message.set(attribute, value);
+		return this;
+	}
+
+	public boolean contains(String attribute) {
+		return message.contains(attribute);
+	}
+
+	public List<String> attributes() {
+		return message.attributes();
+	}
+
+	public List<Message> components() {
+		return message.components();
+	}
+
+	public List<Message> components(String type) {
+		return message.components(type);
+	}
+
+	public void add(Message component) {
+		message.add(component);
 	}
 
 	public SessionEvent ts(Instant ts) {
