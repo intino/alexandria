e44048f281d6678b2b7a9fbcb3316fa471ecdfef
Octavio Roncal <octavioroncal@monentia.es>
2020-05-18 07:49:32 +0100

    updated tests

--- fileformats/zim/test/io/intino/test/ZimReader_.java
+++ fileformats/zim/test/io/intino/test/ZimReader_.java
@@ -6,6 +6,8 @@ import io.intino.alexandria.zim.ZimStream;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.time.Instant;
 import java.util.TimeZone;
 
@@ -22,20 +24,20 @@ public class ZimReader_ {
 	public void should_parse_multiple_messages() {
 		String inl =
 				"[Status]\n" +
-				"battery: 78.0\n" +
-				"cpuUsage: 11.95\n" +
-				"isPlugged: true\n" +
-				"isScreenOn: false\n" +
-				"temperature: 29.0\n" +
-				"created: 2017-03-22T12:56:18Z\n" +
-                "\n" +
-                "[Status]\n" +
-				"battery: 78.0\n" +
-				"cpuUsage: 11.95\n" +
-				"isPlugged: true\n" +
-				"isScreenOn: true\n" +
-				"temperature: 29.0\n" +
-				"created: 2017-03-22T12:56:18Z\n";
+						"battery: 78.0\n" +
+						"cpuUsage: 11.95\n" +
+						"isPlugged: true\n" +
+						"isScreenOn: false\n" +
+						"temperature: 29.0\n" +
+						"created: 2017-03-22T12:56:18Z\n" +
+						"\n" +
+						"[Status]\n" +
+						"battery: 78.0\n" +
+						"cpuUsage: 11.95\n" +
+						"isPlugged: true\n" +
+						"isScreenOn: true\n" +
+						"temperature: 29.0\n" +
+						"created: 2017-03-22T12:56:18Z\n";
 
 		ZimStream zimStream = new ZimReader(inl);
 		Message[] messages = new Message[3];
@@ -56,15 +58,15 @@ public class ZimReader_ {
 	@Test
 	public void should_ignore_empty_attributes() {
 		String inl =
-			"[Teacher]\n" +
-			"name: Jose\n" +
-			"money: 50.0\n" +
-			"birthDate: 2016-10-04T20:10:11Z\n" +
-			"university: ULPGC\n" +
-			"\n" +
-			"[Person.Country]\n" +
-			"name: Spain\n" +
-			"continent:\n";
+				"[Teacher]\n" +
+						"name: Jose\n" +
+						"money: 50.0\n" +
+						"birthDate: 2016-10-04T20:10:11Z\n" +
+						"university: ULPGC\n" +
+						"\n" +
+						"[Person.Country]\n" +
+						"name: Spain\n" +
+						"continent:\n";
 
 		Message message = new ZimReader(inl).next();
 		assertThat(message.is("teacher")).isTrue();
@@ -75,33 +77,41 @@ public class ZimReader_ {
 		assertThat(message.components("country").get(0).contains("continent")).isFalse();
 		assertThat(message.components("country").get(0).get("name").as(String.class)).isEqualTo("Spain");
 		assertThat(message.components("country").get(0).get("continent").as(String.class)).isNull();
+	}
 
+	@Test
+	public void should_read_messages_with_list() throws FileNotFoundException {
+		ZimReader zimReader = new ZimReader(new FileInputStream("test-res/b.zim"));
+		while (zimReader.hasNext()) {
+			Message next = zimReader.next();
+			System.out.println(next.toString());
+		}
 	}
 
 	@Test
 	public void should_parse_multiline_attributes() {
-        String stack =
-                "java.lang.NullPointerException: Attempt to invoke interface method 'java.lang.Object java.util.List.get(int)' on a null object reference\n" +
-                "    at io.intino.consul.AppService$5.run(AppService.java:154)\n" +
-                "    at android.os.Handler.handleCallback(Handler.java:815)\n" +
-                "    at android.os.Handler.dispatchMessage(Handler.java:104)\n" +
-                "    at android.os.Looper.loop(Looper.java:194)\n" +
-                "    at android.app.ActivityThread.main(ActivityThread.java:5666)\n" +
-                "    at java.lang.reflect.Method.invoke(Native Method)\n" +
-                "    at java.lang.reflect.Method.invoke(Method.java:372)\n" +
-                "\n" +
-                "    at com.android.compiler.os.ZygoteInit$MethodAndArgsCaller.run(ZygoteInit.java:959)\n" +
-                "    at com.android.compiler.os.ZygoteInit.main(ZygoteInit.java:754)" +
-                "\n" +
-                "\n";
-        String inl =
-                "[Crash]\n" +
-                "instant: 2017-03-21T07:39:00Z\n" +
-                "app: io.intino.consul\n" +
-                "deviceId: b367172b0c6fe726\n" +
-                "stack:\n" + indent(stack) + "\n";
+		String stack =
+				"java.lang.NullPointerException: Attempt to invoke interface method 'java.lang.Object java.util.List.get(int)' on a null object reference\n" +
+						"    at io.intino.consul.AppService$5.run(AppService.java:154)\n" +
+						"    at android.os.Handler.handleCallback(Handler.java:815)\n" +
+						"    at android.os.Handler.dispatchMessage(Handler.java:104)\n" +
+						"    at android.os.Looper.loop(Looper.java:194)\n" +
+						"    at android.app.ActivityThread.main(ActivityThread.java:5666)\n" +
+						"    at java.lang.reflect.Method.invoke(Native Method)\n" +
+						"    at java.lang.reflect.Method.invoke(Method.java:372)\n" +
+						"\n" +
+						"    at com.android.compiler.os.ZygoteInit$MethodAndArgsCaller.run(ZygoteInit.java:959)\n" +
+						"    at com.android.compiler.os.ZygoteInit.main(ZygoteInit.java:754)" +
+						"\n" +
+						"\n";
+		String inl =
+				"[Crash]\n" +
+						"instant: 2017-03-21T07:39:00Z\n" +
+						"app: io.intino.consul\n" +
+						"deviceId: b367172b0c6fe726\n" +
+						"stack:\n" + indent(stack) + "\n";
 
-        Message message = new ZimReader(inl).next();
+		Message message = new ZimReader(inl).next();
 		assertThat(message.type()).isEqualTo("Crash");
 		assertThat(message.contains("instant")).isTrue();
 		assertThat(message.contains("app")).isTrue();
@@ -115,29 +125,29 @@ public class ZimReader_ {
 
 	@Test
 	public void should_parse_message_with_multiple_components() {
-        String inl =
-                "[Teacher]\n" +
-            "name: Jose\n" +
-            "money: 50.0\n" +
-            "birthDate: 2016-10-04T20:10:11Z\n" +
-            "university: ULPGC\n" +
-            "\n" +
-            "[Teacher.Country]\n" +
-            "name: Spain\n" +
-            "\n" +
-            "[Teacher.Phone]\n" +
-            "value: +150512101402\n" +
-            "\n" +
-            "[Teacher.Phone.Country]\n" +
-            "name: USA\n" +
-            "\n" +
-            "[Teacher.Phone]\n" +
-            "value: +521005101402\n" +
-            "\n" +
-            "[Teacher.Phone.Country]\n" +
-            "name: Mexico\n";
+		String inl =
+				"[Teacher]\n" +
+						"name: Jose\n" +
+						"money: 50.0\n" +
+						"birthDate: 2016-10-04T20:10:11Z\n" +
+						"university: ULPGC\n" +
+						"\n" +
+						"[Teacher.Country]\n" +
+						"name: Spain\n" +
+						"\n" +
+						"[Teacher.Phone]\n" +
+						"value: +150512101402\n" +
+						"\n" +
+						"[Teacher.Phone.Country]\n" +
+						"name: USA\n" +
+						"\n" +
+						"[Teacher.Phone]\n" +
+						"value: +521005101402\n" +
+						"\n" +
+						"[Teacher.Phone.Country]\n" +
+						"name: Mexico\n";
 
-        Message message = new ZimReader(inl).next();
+		Message message = new ZimReader(inl).next();
 		assertThat(message.type()).isEqualTo("Teacher");
 		assertThat(message.components("country").size()).isEqualTo(1);
 		assertThat(message.components("country").get(0).type()).isEqualTo("Country");
@@ -146,9 +156,9 @@ public class ZimReader_ {
 		assertThat(message.toString()).isEqualTo(inl);
 	}
 
-    private static String indent(String text) {
-        return "\t" + text.replaceAll("\\n", "\n\t");
-    }
+	private static String indent(String text) {
+		return "\t" + text.replaceAll("\\n", "\n\t");
+	}
 
 
 }
