9431c315c1b39ebf49b89b40198633f64e20345a
Octavio Roncal <octavioroncal@monentia.es>
2020-06-15 11:24:33 +0100

    updated subscriber template

--- toolbox/builder/src/io/intino/konos/builder/codegeneration/datahub/subscriber/SubscriberTemplate.java
+++ toolbox/builder/src/io/intino/konos/builder/codegeneration/datahub/subscriber/SubscriberTemplate.java
@@ -7,7 +7,7 @@ public class SubscriberTemplate extends Template {
 
 	public RuleSet ruleSet() {
 		return new RuleSet().add(
-			rule().condition((type("subscriber"))).output(literal("package ")).output(mark("package", "validPackage")).output(literal(".subscribers;\n\nimport ")).output(mark("package", "validPackage")).output(literal(".")).output(mark("box", "firstUpperCase")).output(literal("Box;\n\npublic class ")).output(mark("name", "FirstUpperCase")).output(literal(" implements java.util.function.Consumer<")).output(mark("type")).output(literal("> {\n\tprivate ")).output(mark("box", "validName", "firstUpperCase")).output(literal("Box box;\n\n\tpublic ")).output(mark("name", "FirstUpperCase")).output(literal("(")).output(mark("box", "validName", "firstUpperCase")).output(literal("Box box) {\n\t\tthis.box = box;\n\t}\n\n\tpublic void accept(")).output(mark("type")).output(literal(" event) {\n\n\t}\n}"))
+				rule().condition((type("subscriber"))).output(literal("package ")).output(mark("package", "validPackage")).output(literal(".subscribers;\n\nimport ")).output(mark("package", "validPackage")).output(literal(".")).output(mark("box", "firstUpperCase")).output(literal("Box;\n\npublic class ")).output(mark("name", "FirstUpperCase")).output(literal(" implements java.util.function.Consumer<")).output(mark("type")).output(literal("> {\n\tprivate final ")).output(mark("box", "validName", "firstUpperCase")).output(literal("Box box;\n\n\tpublic ")).output(mark("name", "FirstUpperCase")).output(literal("(")).output(mark("box", "validName", "firstUpperCase")).output(literal("Box box) {\n\t\tthis.box = box;\n\t}\n\n\tpublic void accept(")).output(mark("type")).output(literal(" event) {\n\n\t}\n}"))
 		);
 	}
 }
