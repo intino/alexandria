deebbfd91e4ab896aa94c8e4bfad0dd5af5faeb8
Mario Caballero Ramírez <mariocaballero@siani.es>
2020-07-14 18:58:07 +0100

    alexandria-ui: Added frame empty display option

--- framework/ui/src/io/intino/alexandria/ui/services/push/PushService.java
+++ framework/ui/src/io/intino/alexandria/ui/services/push/PushService.java
@@ -1,6 +1,7 @@
 package io.intino.alexandria.ui.services.push;
 
 import io.intino.alexandria.Json;
+import io.intino.alexandria.http.pushservice.Client;
 import io.intino.alexandria.logger.Logger;
 import io.intino.alexandria.ui.displays.requesters.DisplayPushRequester;
 import org.eclipse.jetty.io.EofException;
@@ -10,9 +11,11 @@ import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Consumer;
 
 public class PushService extends io.intino.alexandria.http.spark.PushService<UISession, UIClient> {
 	private Map<String, DisplayPushRequester> requesterMap = new HashMap<>();
+	private static final Map<String, Consumer<Long>> linkedListeners = new HashMap<>();
 
 	@Override
 	public UISession createSession(String id) {
@@ -22,6 +25,12 @@ public class PushService extends io.intino.alexandria.http.spark.PushService<UIS
 	@Override
 	public UIClient createClient(Session session) {
 		return new UIClient(session);
+	}
+
+	@Override
+	public void linkToThread(UIClient client) {
+		super.linkToThread(client);
+		if (linkedListeners.containsKey(client.id())) linkedListeners.get(client.id()).accept(Thread.currentThread().getId());
 	}
 
 	@Override
@@ -43,6 +52,16 @@ public class PushService extends io.intino.alexandria.http.spark.PushService<UIS
 			if (e instanceof EofException) Logger.debug(e.getMessage());
 			else Logger.error(e);
 		}
+	}
+
+	@Override
+	public void onClose(UIClient client) {
+		super.onClose(client);
+		linkedListeners.remove(client.id());
+	}
+
+	public void onLinkedToThread(Client client, Consumer<Long> listener) {
+		linkedListeners.put(client.id(), listener);
 	}
 
 	public PushService register(String type, DisplayPushRequester requester) {
