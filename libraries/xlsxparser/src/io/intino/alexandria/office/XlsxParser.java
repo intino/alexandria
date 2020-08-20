3f60da749c93b3f1de7ae54217e0fabce8e23085
jevora <legendus@gmail.com>
2020-08-20 08:38:39 +0100

    added example for xlsxbuilder

--- libraries/xlsxparser/src/io/intino/alexandria/office/XlsxParser.java
+++ libraries/xlsxparser/src/io/intino/alexandria/office/XlsxParser.java
@@ -4,41 +4,42 @@ import org.apache.poi.ss.usermodel.*;
 
 import java.io.*;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import static java.lang.String.valueOf;
 
 public class XlsxParser {
 
-	public Map<String, String> convert(File file) throws IOException {
+	public static Map<String, String> convert(File file) throws IOException {
 		return convert(WorkbookFactory.create(file));
 	}
 
-	public Map<String, String> convert(File file, String password) throws IOException {
+	public static Map<String, String> convert(File file, String password) throws IOException {
 		return convert(WorkbookFactory.create(file, password));
 	}
 
-	private HashMap<String, String> convert(Workbook wb) {
-		HashMap<String, String> result = new HashMap<>();
+	private static Map<String, String> convert(Workbook wb) {
+		Map<String, String> result = new LinkedHashMap<>();
 		for (Sheet sheet : wb)
 			result.put(sheet.getSheetName(), convert(sheet));
 		return result;
 	}
 
-	private String convert(Sheet sheet) {
+	private static String convert(Sheet sheet) {
 		StringBuilder data = new StringBuilder();
 		for (Row row : sheet) data.append(convert(row));
 		return data.toString();
 	}
 
-	private String convert(Row row) {
+	private static String convert(Row row) {
 		StringBuilder data = new StringBuilder();
 		for (Cell cell : row) data.append('\t').append(convert(cell));
 		data.append('\n');
 		return data.substring(1);
 	}
 
-	private String convert(Cell cell) {
+	private static String convert(Cell cell) {
 		switch (cell.getCellType()) {
 			case STRING: return cell.getStringCellValue();
 			case BOOLEAN: return valueOf(cell.getBooleanCellValue());
