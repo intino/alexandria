f9a64bb5b62f02c330d5befb60b811e8c25ceb78
Octavio Roncal <octavioroncal@monentia.es>
2020-10-19 08:27:07 +0100

    updated tests

--- libraries/ness/ingestion/src/io/intino/alexandria/ingestion/LedSession.java
+++ libraries/ness/ingestion/src/io/intino/alexandria/ingestion/LedSession.java
@@ -3,7 +3,7 @@ package io.intino.alexandria.ingestion;
 import io.intino.alexandria.Fingerprint;
 import io.intino.alexandria.Session;
 import io.intino.alexandria.Timetag;
-import io.intino.alexandria.led.LedStream;
+import io.intino.alexandria.led.Led;
 import io.intino.alexandria.led.LedWriter;
 
 public class LedSession {
@@ -13,7 +13,7 @@ public class LedSession {
 		this.provider = provider;
 	}
 
-	public void put(String tank, Timetag timetag, LedStream<?> led) {
+	public void put(String tank, Timetag timetag, Led<?> led) {
 		new LedWriter(provider.outputStream(Fingerprint.of(tank, timetag).name(), Session.Type.led)).write(led);
 	}
 }
--- libraries/ness/sealing/test/io/intino/alexandria/sealing/LedSessionManager_.java
+++ libraries/ness/sealing/test/io/intino/alexandria/sealing/LedSessionManager_.java
@@ -9,22 +9,26 @@ import io.intino.alexandria.led.Led;
 import io.intino.alexandria.led.LedStream;
 import io.intino.alexandria.led.LedReader;
 import io.intino.alexandria.led.Schema;
+import io.intino.alexandria.led.allocators.SchemaAllocator;
+import io.intino.alexandria.led.allocators.stack.StackAllocators;
 import io.intino.alexandria.led.leds.ListLed;
 import org.junit.After;
 import org.junit.Test;
 
 import java.io.File;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.Iterator;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
 
 public class LedSessionManager_ {
 	private static final File LOCAL_STAGE = new File("temp/localstage");
 	private static final File STAGE_FOLDER = new File("temp/stage");
 	private static final File DATALAKE = new File("temp/datalake");
+
 
 	@Test
 	public void should_create_and_seal_led_session() {
@@ -32,16 +36,27 @@ public class LedSessionManager_ {
 		LocalDateTime dateTime = LocalDateTime.of(2019, 2, 28, 16, 15);
 		Timetag timetag = new Timetag(dateTime, Scale.Day);
 		LedSession session = handler.createLedSession();
-		List<TestSchema> stored = TestSchema.unsortedList();
+		List<TestSchema> stored = unsortedList();
 		stored.sort(Comparator.comparingLong(Schema::id));
 		Led<TestSchema> testLed = new ListLed<>(stored);
 		session.put("tank1", timetag, testLed);
 		handler.pushTo(STAGE_FOLDER);
 		new FileSessionSealer(new FileDatalake(DATALAKE), STAGE_FOLDER).seal();
-		LedStream<TestSchema> led = new LedReader(new File("temp/datalake/ledger/tank1/" + timetag.value() + ".led")).read(TestSchema::new);
-		assertThat(led.iterator().next().id()).isEqualTo(testLed.iterator().next().id());
-		assertThat(led.iterator().next().e()).isEqualTo(testLed.iterator().next().e());
-		assertThat(led.iterator().next().d()).isEqualTo(testLed.iterator().next().d());
+		LedStream<TestSchema> stream = new LedReader(new File("temp/datalake/transactions/tank1/" + timetag.value() + ".led")).read(TestSchema::new);
+		Iterator<TestSchema> iterator = testLed.iterator();
+		assertThat(stream.next().id()).isEqualTo(iterator.next().id());
+		assertThat(stream.next().e()).isEqualTo(iterator.next().e());
+		assertThat(stream.next().d()).isEqualTo(iterator.next().d());
+	}
+
+	private List<TestSchema> unsortedList() {
+		SchemaAllocator<TestSchema> allocator = StackAllocators.newManaged(TestSchema.SIZE, 1000, TestSchema::new);
+		List<TestSchema> result = new ArrayList<>();
+		for (int i = 10; i <= 1000; i += 5) {
+			int id = (int) Math.cos(i / 20 * Math.PI) * i;
+			result.add(allocator.calloc().id(id).b(i - 500).f(i * 100.0 / 20.0));
+		}
+		return result;
 	}
 
 	@After
