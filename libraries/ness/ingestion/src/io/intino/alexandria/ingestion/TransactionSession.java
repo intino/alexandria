6e7919258f75bfe0048cf8ecd4617d941861a27d
Octavio Roncal <octavioroncal@monentia.es>
2020-10-26 13:52:33 +0000

    updated TransactionSession

--- libraries/ness/ingestion/src/io/intino/alexandria/ingestion/TransactionSession.java
+++ libraries/ness/ingestion/src/io/intino/alexandria/ingestion/TransactionSession.java
@@ -4,6 +4,7 @@ import io.intino.alexandria.Fingerprint;
 import io.intino.alexandria.Session;
 import io.intino.alexandria.Timetag;
 import io.intino.alexandria.led.Led;
+import io.intino.alexandria.led.LedStream;
 import io.intino.alexandria.led.LedWriter;
 
 public class TransactionSession {
@@ -15,5 +16,9 @@ public class TransactionSession {
 
 	public void put(String tank, Timetag timetag, Led<?> led) {
 		new LedWriter(provider.outputStream(Fingerprint.of(tank, timetag).name(), Session.Type.led)).write(led);
+	}
+
+	public void put(String tank, Timetag timetag, LedStream<?> stream) {
+		new LedWriter(provider.outputStream(Fingerprint.of(tank, timetag).name(), Session.Type.led)).write(stream);
 	}
 }
