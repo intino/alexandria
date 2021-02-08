76d94e2569c1f0dbfb1199230fe088eeb92fe02a
Cristian Herrera <naitsirc98@gmail.com>
2021-02-08 13:30:10 +0000

    Add generic attributes annotations and proper toString for schemas

--- fileformats/led/src/io/intino/alexandria/led/Schema.java
+++ fileformats/led/src/io/intino/alexandria/led/Schema.java
@@ -8,13 +8,18 @@ import io.intino.alexandria.led.buffers.store.ByteStore;
 import io.intino.alexandria.led.util.memory.MemoryUtils;
 import io.intino.alexandria.led.util.OffHeapObject;
 import io.intino.alexandria.logger.Logger;
+import org.apache.commons.lang3.StringUtils;
 
+import javax.xml.crypto.Data;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.lang.reflect.Constructor;
-import java.util.Objects;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static java.nio.ByteOrder.*;
 
@@ -137,5 +142,90 @@ public abstract class Schema implements OffHeapObject, Comparable<Schema> {
 
 	public String toHexString() {
 		return bitBuffer.toHexString();
+	}
+
+	@Override
+	public String toString() {
+		return toString(false);
+	}
+
+	public String toString(boolean verbose) {
+		Class<? extends Schema> clazz = getClass();
+		SchemaDefinition definition = clazz.getAnnotation(SchemaDefinition.class);
+		if(definition == null) {
+			return super.toString();
+		}
+
+		StringBuilder sb = new StringBuilder();
+		sb.append(definition.name());
+		if(verbose) {
+			sb.append("(").append(definition.size()).append(" bytes)");
+		}
+		sb.append(" {");
+
+		final Method[] methods = Arrays.stream(clazz.getMethods())
+				.filter(m -> m.isAnnotationPresent(Attribute.class))
+				.sorted(Comparator.comparingInt(m -> m.getAnnotation(Attribute.class).index()))
+				.toArray(Method[]::new);
+
+		for(int i = 0;i < methods.length;i++) {
+			Method method = methods[i];
+			Attribute attribute = method.getAnnotation(Attribute.class);
+			Object value;
+			try {
+				value = method.invoke(this);
+			} catch (Throwable ignored) {
+				value = null;
+			}
+			if(verbose) {
+				sb.append('[').append(attribute.type().typename())
+						.append(',').append(attribute.index())
+						.append(',').append(attribute.size()).append("] ");
+			}
+			sb.append(attribute.name()).append(" = ").append(value);
+			if(i < methods.length - 1) sb.append(", ");
+		}
+		return sb.append('}').toString();
+	}
+
+	public enum DataType {
+
+		BYTE(Byte.SIZE),
+		UNSIGNED_BYTE(Byte.SIZE),
+		SHORT(Short.SIZE),
+		UNSIGNED_SHORT(Short.SIZE),
+		INT(Integer.SIZE),
+		UNSIGNED_INT(Integer.SIZE),
+		LONG(Long.SIZE),
+		UNSIGNED_LONG(Long.SIZE - 1),
+		FLOAT(Float.SIZE),
+		DOUBLE(Double.SIZE),
+		CATEGORY(Long.SIZE);
+
+		public final int maxSize;
+
+		DataType(int maxSize) {
+			this.maxSize = maxSize;
+		}
+
+		public String typename() {
+			return name().toLowerCase();
+		}
+	}
+
+	@Retention(RetentionPolicy.RUNTIME)
+	@Target(ElementType.METHOD)
+	public @interface Attribute {
+		String name();
+		DataType type();
+		int index();
+		int size();
+	}
+
+	@Retention(RetentionPolicy.RUNTIME)
+	@Target(ElementType.TYPE)
+	public @interface SchemaDefinition {
+		String name();
+		int size();
 	}
 }
--- fileformats/led/test/io/intino/test/schemas/VentaEnergia.java
+++ fileformats/led/test/io/intino/test/schemas/VentaEnergia.java
@@ -1,6 +1,7 @@
 package io.intino.test.schemas;
 
 import io.intino.alexandria.led.Schema;
+import io.intino.alexandria.led.Schema.SchemaDefinition;
 import io.intino.alexandria.led.buffers.store.ByteBufferStore;
 import io.intino.alexandria.led.buffers.store.ByteStore;
 import io.intino.alexandria.led.util.memory.MemoryAddress;
@@ -10,7 +11,16 @@ import java.nio.ByteBuffer;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+@SchemaDefinition(name = "VentaEnergia", size = VentaEnergia.SIZE)
 public class VentaEnergia extends Schema {
+
+    public static void main(String[] args) {
+
+        VentaEnergia ventaEnergia = new VentaEnergia().claseVenta(1).concepto(3).total(12345).ocr(98765);
+
+        System.out.println(ventaEnergia);
+
+    }
 
     public static final int SIZE = 52;
 
@@ -40,42 +50,52 @@ public class VentaEnergia extends Schema {
         return values;
     }
 
+    @Override
     protected long id() {
         return this.ocr();
     }
 
+    @Attribute(name = "total", type = DataType.LONG, index = 0, size = Long.SIZE)
     public long total() {
         return this.bitBuffer.getAlignedLong(0);
     }
 
+    @Attribute(name = "dap", type = DataType.LONG, index = 64, size = Long.SIZE)
     public long dap() {
         return this.bitBuffer.getAlignedLong(64);
     }
 
+    @Attribute(name = "iva", type = DataType.LONG, index = 128, size = Long.SIZE)
     public long iva() {
         return this.bitBuffer.getAlignedLong(128);
     }
 
+    @Attribute(name = "importe", type = DataType.LONG, index = 192, size = Long.SIZE)
     public long importe() {
         return this.bitBuffer.getLongNBits(192, 64);
     }
 
+    @Attribute(name = "kwh", type = DataType.LONG, index = 256, size = Long.SIZE)
     public long kwh() {
         return this.bitBuffer.getAlignedLong(256);
     }
 
+    @Attribute(name = "ocr", type = DataType.LONG, index = 320, size = Long.SIZE)
     public long ocr() {
         return this.bitBuffer.getLongNBits(320, 64);
     }
 
+    @Attribute(name = "diasFacturados", type = DataType.LONG, index = 384, size = 16)
     public int diasFacturados() {
         return this.bitBuffer.getIntegerNBits(384, 16);
     }
 
+    @Attribute(name = "claseVenta", type = DataType.LONG, index = 400, size = 8)
     public int claseVenta() {
         return this.bitBuffer.getIntegerNBits(400, 8);
     }
 
+    @Attribute(name = "concepto", type = DataType.LONG, index = 408, size = 7)
     public int concepto() {
         return this.bitBuffer.getIntegerNBits(408, 7);
     }
