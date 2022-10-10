f0451bedf29db299393c09a80cf409d9bba00343
Octavio Roncal <octavioroncal@monentia.com>
2022-10-10 12:52:44 +0100

    builder: updated mounters

--- toolbox/builder/src/io/intino/konos/builder/codegeneration/datahub/mounter/MounterRenderer.java
+++ toolbox/builder/src/io/intino/konos/builder/codegeneration/datahub/mounter/MounterRenderer.java
@@ -10,9 +10,6 @@ import io.intino.konos.model.Datamart.Mounter;
 import io.intino.konos.model.KonosGraph;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.Objects;
 
 import static io.intino.konos.builder.codegeneration.Formatters.customize;
@@ -37,9 +34,7 @@ public class MounterRenderer {
 			for (Mounter mounter : datamart.mounterList()) {
 				final String mounterName = mounter.name$();
 				final FrameBuilder builder = baseFrame(mounter);
-				if (mounter.isPopulation())
-					populationMounter(mounter, mounterName, builder);
-				else if (manifest != null && !alreadyRendered(context.src(Target.Owner), mounterName))
+				if (mounter.isEvent() && manifest != null && !alreadyRendered(context.src(Target.Owner), mounterName))
 					eventMounter(mounter, mounterName, manifest, builder);
 			}
 	}
@@ -65,56 +60,6 @@ public class MounterRenderer {
 
 	private String[] types(Mounter mounter, CompilationContext.DataHubManifest manifest) {
 		return mounter.asEvent().requireList().stream().map(r -> manifest.tankClasses.get(r.tank())).filter(Objects::nonNull).toArray(String[]::new);
-	}
-
-	private void populationMounter(Mounter mounter, String mounterName, FrameBuilder builder) {
-		populationMounter(builder, mounter.asPopulation());
-		context.classes().put(mounter.getClass().getSimpleName() + "#" + mounter.name$(), "mounters." + mounterName);
-		writeFrame(genMounters, mounter.name$(), customize(new MounterTemplate()).render(builder.toFrame()));
-		context.compiledFiles().add(new OutputItem(context.sourceFileOf(mounter), javaFile(genMounters, mounter.name$()).getAbsolutePath()));
-		mounterFunctions(mounter);
-	}
-
-	private void mounterFunctions(Mounter mounter) {
-		String datamart = mounter.core$().ownerAs(Datamart.class).name$();
-		mounter.asPopulation().clear().split(t -> true);
-		FrameBuilder baseFrame = baseFrame(mounter).add("src");
-		populationMounter(baseFrame, mounter.asPopulation());
-		File datamartFolder = new File(context.src(Target.Owner), datamart);
-		File mounters = new File(datamartFolder, "mounters");
-		if (!alreadyRendered(mounters, mounter.name$() + "MounterFunctions"))
-			writeFrame(mounters, mounter.name$() + "MounterFunctions", customize(new MounterTemplate()).render(baseFrame.toFrame()));
-	}
-
-	private void populationMounter(FrameBuilder builder, Mounter.Population mounter) {
-		List<FrameBuilder> frames = new ArrayList<>();
-		mounter.sourceList().stream().map(this::buildersOf).forEach(frames::addAll);
-		builder.add("population").
-				add("column", frames.toArray(new FrameBuilder[0])).
-				add("datamart", mounter.core$().ownerAs(Datamart.class).name$()).
-				add("format", mounter.format().name());
-	}
-
-	private List<FrameBuilder> buildersOf(Mounter.Population.Source source) {
-		List<FrameBuilder> builders = new ArrayList<>();
-		for (String tank : tanksOf(source.core$().ownerAs(Mounter.Population.class), source.tank())) {
-			FrameBuilder builder = new FrameBuilder("column")
-					.add("fullName", tank)
-					.add("name", source.tank())
-					.add("type", source.type().name())
-					.add("mounter", source.core$().ownerAs(Mounter.class).name$());
-			if (source.isId()) builder.add("facet", "id");
-			builders.add(builder);
-		}
-		return builders;
-	}
-
-	private List<String> tanksOf(Mounter.Population mounter, String tank) {
-		List<String> tanks = new ArrayList<>();
-		if (!mounter.splitList().isEmpty()) for (Mounter.Population.Split split : mounter.splitList())
-			split.splits().stream().map(s -> s + "." + tank).forEach(tanks::add);
-		else return Collections.singletonList(tank);
-		return tanks;
 	}
 
 	private boolean alreadyRendered(File destination, String action) {
