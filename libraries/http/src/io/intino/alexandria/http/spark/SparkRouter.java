c21234c5b2d8f5ea4d8e4602f478086ce2d697a5
Octavio Roncal <octavioroncal@monentia.com>
2022-11-02 15:21:28 +0000

    fixed schema annotated serialization

--- libraries/http/src/io/intino/alexandria/http/spark/SparkRouter.java
+++ libraries/http/src/io/intino/alexandria/http/spark/SparkRouter.java
@@ -3,6 +3,7 @@ package io.intino.alexandria.http.spark;
 import io.intino.alexandria.exceptions.AlexandriaException;
 import io.intino.alexandria.http.AlexandriaSpark;
 import io.intino.alexandria.logger.Logger;
+import spark.HaltException;
 import spark.Request;
 import spark.Response;
 import spark.Service;
@@ -114,6 +115,8 @@ public class SparkRouter<SM extends SparkManager> {
 			manager.response.status(Integer.parseInt(e.code()));
 			manager.response.body(e.toString());
 			service.halt(Integer.parseInt(e.code()), e.toString());
+		} catch (HaltException e) {
+			service.halt(e.statusCode());
 		} catch (Throwable e) {
 			Logger.error(e);
 			manager.response.status(500);
