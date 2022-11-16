580561bc2909d07c04b621ffa0448f9ea36e2431
cherrera <naitsirc98@gmail.com>
2022-11-16 12:22:41 +0000

    json: bug fix

--- fileformats/json/src/io/intino/alexandria/Json.java
+++ fileformats/json/src/io/intino/alexandria/Json.java
@@ -11,47 +11,41 @@ import java.util.Date;
 
 public class Json {
 
+	private static final Gson GsonInstance = gsonBuilder().create();
+	private static final Gson GsonPrettyInstance = gsonBuilder().setPrettyPrinting().create();
+
 	public static String toString(Object object) {
 		return toJson(object);
 	}
 
 	public static String toStringPretty(Object object) {
-		return toJsonPretty(object);
+		return toJson(object);
 	}
 
 	public static String toJson(Object object) {
-		return gsonWriter().toJson(object);
+		return GsonInstance.toJson(object);
 	}
 
 	public static String toJsonPretty(Object object) {
-		return gsonWriterPretty().toJson(object);
+		return GsonPrettyInstance.toJson(object);
 	}
 
-	public static <T> T fromString(String text, Class<T> type) {
-		return gsonReader().fromJson(text, type);
+	public static <T> T fromString(String json, Class<T> type) {
+		return GsonInstance.fromJson(json, type);
 	}
 
-	public static <T> T fromJson(String text, Class<T> type) {
-		return gsonReader().fromJson(text, type);
-	}
-
-	public static Gson gsonReader() {
-		return gsonBuilder().create();
-	}
-
-	public static Gson gsonWriter() {
-		return gsonBuilder().create();
-	}
-
-	public static Gson gsonWriterPretty() {
-		return gsonBuilder().setPrettyPrinting().create();
+	public static <T> T fromJson(String json, Class<T> type) {
+		return GsonInstance.fromJson(json, type);
 	}
 
 	private static GsonBuilder gsonBuilder() {
-		return new GsonBuilder().
-				registerTypeAdapter(Instant.class, (JsonSerializer<Instant>) (instant, type, context) -> new JsonPrimitive(instant.toEpochMilli())).
-				registerTypeAdapter(Date.class, (JsonSerializer<Date>) (date, type, context) -> new JsonPrimitive(date.getTime())).
-				registerTypeAdapter(InputStream.class, (JsonSerializer<InputStream>) (inputStream, type1, jsonDeserializationContext) -> new JsonPrimitive(Base64.encode(toByteArray(inputStream))));
+		return new GsonBuilder()
+				.registerTypeAdapter(Instant.class, (JsonSerializer<Instant>) (instant, type, context) -> new JsonPrimitive(instant.toEpochMilli()))
+				.registerTypeAdapter(Instant.class, (JsonDeserializer<Instant>) (json, type1, context) -> Instant.ofEpochMilli(json.getAsJsonPrimitive().getAsLong()))
+				.registerTypeAdapter(Date.class, (JsonSerializer<Date>) (date, type, context) -> new JsonPrimitive(date.getTime()))
+				.registerTypeAdapter(Date.class, (JsonDeserializer<Date>) (json, type1, context) -> new Date(json.getAsJsonPrimitive().getAsLong()))
+				.registerTypeAdapter(InputStream.class, (JsonSerializer<InputStream>) (inputStream, type1, jsonDeserializationContext) -> new JsonPrimitive(Base64.encode(toByteArray(inputStream))))
+				.registerTypeAdapter(InputStream.class, (JsonDeserializer<InputStream>) (json, type1, context) -> new ByteArrayInputStream(Base64.decode(json.getAsString())));
 	}
 
 	private static byte[] toByteArray(InputStream is) {
