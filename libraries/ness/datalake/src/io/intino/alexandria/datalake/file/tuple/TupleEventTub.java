3f36815128044d11a76845b45095ff2d304f280f
cherrera <naitsirc98@gmail.com>
2023-02-20 13:05:58 +0000

    Use EventStream in TupleEventTub

--- libraries/ness/datalake/src/io/intino/alexandria/datalake/file/tuple/TupleEventTub.java
+++ libraries/ness/datalake/src/io/intino/alexandria/datalake/file/tuple/TupleEventTub.java
@@ -3,13 +3,12 @@ package io.intino.alexandria.datalake.file.tuple;
 import io.intino.alexandria.Timetag;
 import io.intino.alexandria.datalake.Datalake;
 import io.intino.alexandria.event.Event;
+import io.intino.alexandria.event.EventStream;
 import io.intino.alexandria.event.tuple.TupleEvent;
 import io.intino.alexandria.logger.Logger;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class TupleEventTub implements Datalake.Store.Tub<TupleEvent> {
@@ -30,9 +29,9 @@ public class TupleEventTub implements Datalake.Store.Tub<TupleEvent> {
 
 	@Override
 	public Stream<TupleEvent> events() {
-		try (Stream<String> lines = Files.lines(tsv.toPath())) {
-			return lines.filter(line -> !line.isEmpty()).map(l -> new TupleEvent(l.split("\t", -1))).collect(Collectors.toList()).stream();
-		} catch (IOException e) {
+		try {
+			return EventStream.of(tsv);
+		} catch(IOException e) {
 			Logger.error(e);
 			return Stream.empty();
 		}
