4b2ad379b64aa79c739f9a2f5c429a82b15c2f9a
Octavio Roncal <octavioroncal@monentia.com>
2023-02-23 08:22:31 +0000

    event: WIP

--- libraries/ness/event/src/io/intino/alexandria/event/message/MessageEventWriter.java
+++ libraries/ness/event/src/io/intino/alexandria/event/message/MessageEventWriter.java
@@ -1,17 +1,14 @@
 package io.intino.alexandria.event.message;
 
+import com.github.luben.zstd.ZstdOutputStream;
 import io.intino.alexandria.event.AbstractEventWriter;
-import io.intino.alexandria.event.EventStream;
-import io.intino.alexandria.logger.Logger;
 import io.intino.alexandria.message.MessageWriter;
-import org.xerial.snappy.SnappyOutputStream;
 
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Iterator;
-import java.util.UUID;
 import java.util.stream.Stream;
 
 public class MessageEventWriter extends AbstractEventWriter<MessageEvent> {
@@ -23,14 +20,14 @@ public class MessageEventWriter extends AbstractEventWriter<MessageEvent> {
 	@Override
 	protected File merge(Stream<MessageEvent> data) throws IOException {
 		File file = tempFile();
-		try (MessageWriter writer = new MessageWriter(snappy(file))) {
+		try (MessageWriter writer = new MessageWriter(zstd(file))) {
 			Iterator<MessageEvent> events = mergeFileWith(data).iterator();
 			while (events.hasNext()) writer.write(events.next().toMessage());
 		}
 		return file;
 	}
 
-	private SnappyOutputStream snappy(File file) throws IOException {
-		return new SnappyOutputStream(new BufferedOutputStream(new FileOutputStream(file)));
+	private ZstdOutputStream zstd(File file) throws IOException {
+		return new ZstdOutputStream(new BufferedOutputStream(new FileOutputStream(file)));
 	}
 }
