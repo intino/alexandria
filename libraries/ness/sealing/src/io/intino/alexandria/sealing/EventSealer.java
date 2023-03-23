4b15e85ea0440d4069efc860277c1a75160a3ac9
cherrera <naitsirc98@gmail.com>
2023-03-23 14:09:13 +0000

    sealing: add support for sorting files in parallel

--- libraries/ness/sealing/src/io/intino/alexandria/sealing/EventSealer.java
+++ libraries/ness/sealing/src/io/intino/alexandria/sealing/EventSealer.java
@@ -17,20 +17,39 @@ import io.intino.alexandria.sealing.sorters.ResourceEventSorter;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import static java.util.Objects.requireNonNull;
 
 public class EventSealer {
 	private final Map<Event.Format, Datalake.Store<? extends Event>> stores;
 	private final TankNameFilter tankNameFilter;
 	private final File tempFolder;
+	private boolean multithreading;
 
 	public EventSealer(Datalake datalake, TankNameFilter tankNameFilter, File tempFolder) {
+		this(datalake, tankNameFilter, tempFolder, true);
+	}
+
+	public EventSealer(Datalake datalake, TankNameFilter tankNameFilter, File tempFolder, boolean multithreading) {
 		this.stores = Map.of(Event.Format.Message, datalake.messageStore(), Event.Format.Measurement, datalake.measurementStore(), Event.Format.Resource, datalake.resourceStore());
-		this.tankNameFilter = tankNameFilter;
-		this.tempFolder = tempFolder;
+		this.tankNameFilter = requireNonNull(tankNameFilter, "tankNameFilter cannot be null");
+		this.tempFolder = requireNonNull(tempFolder, "tempFolder cannot be null");
+		this.multithreading = multithreading;
+	}
+
+	public EventSealer multithreading(boolean multithreading) {
+		this.multithreading = multithreading;
+		return this;
 	}
 
 	public void seal(Fingerprint fingerprint, List<File> sessions) throws IOException {
@@ -48,15 +67,46 @@ public class EventSealer {
 		try {
 			EventSorter.Factory sorter = sorterFactoryOf(fingerprint.format());
 			if(!tankNameFilter.accepts(fingerprint.tank()) || sorter == null) return Collections.emptyList();
-			for (File file : files) sorter.of(file, tempFolder).sort();
-			return files;
-		} catch (IOException e) {
+			return (shouldSortInParallel(files)) ? parallelSort(sorter, files) : sequentialSort(sorter, files);
+		} catch (Throwable e) {
 			Logger.error(e);
 			return Collections.emptyList();
 		}
 	}
 
-	public EventSorter.Factory sorterFactoryOf(Event.Format format) throws IOException {
+	private boolean shouldSortInParallel(List<File> files) {
+		return multithreading && files.size() > 1 && Runtime.getRuntime().availableProcessors() > 1;
+	}
+
+	private List<File> parallelSort(EventSorter.Factory sorter, List<File> files) throws Throwable {
+		ExecutorService threadPool = Executors.newFixedThreadPool(Math.min(4, Runtime.getRuntime().availableProcessors()));
+		Throwable[] error = new Throwable[1];
+
+		threadPool.invokeAll(files.stream().map(file -> sort(sorter, file, error)).collect(Collectors.toList()));
+		threadPool.shutdown();
+
+		if(error[0] != null) throw error[0];
+
+		return files;
+	}
+
+	private Callable<Void> sort(EventSorter.Factory sorter, File file, Throwable[] error) {
+		return () -> {
+			try {
+				sorter.of(file, tempFolder).sort();
+			} catch (Throwable e) {
+				error[0] = new RuntimeException("Error while sorting " + file + ": " + e.getMessage(), e);
+			}
+			return null;
+		};
+	}
+
+	private List<File> sequentialSort(EventSorter.Factory sorter, List<File> files) throws Throwable {
+		for(File file : files) sorter.of(file, tempFolder).sort();
+		return files;
+	}
+
+	public EventSorter.Factory sorterFactoryOf(Event.Format format) {
 		switch(format) {
 			case Message: return MessageEventSorter::new;
 //			case Measurement: return new MessageEventSorter(); TODO?
