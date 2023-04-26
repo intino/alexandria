ef24b09451f92c7c4117f3b594c864ce60e3cca3
Octavio Roncal <octavioroncal11@gmail.com>
2023-04-26 07:52:05 +0100

    updated stub connection

--- messaging/terminal-jms/src/io/intino/alexandria/terminal/ConnectorFactory.java
+++ messaging/terminal-jms/src/io/intino/alexandria/terminal/ConnectorFactory.java
@@ -7,7 +7,7 @@ import java.io.File;
 public class ConnectorFactory {
 
 	public static Connector createConnector(ConnectionConfig config, File outboxDirectory) {
-		if (config.url().equals("test")) return new StubConnector(outboxDirectory);
+		if (config.url().startsWith("test")) return new StubConnector(config, outboxDirectory);
 		return new JmsConnector(config, outboxDirectory);
 	}
 }
