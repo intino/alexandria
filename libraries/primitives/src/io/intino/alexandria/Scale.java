fbee4ca2acdc190f39a5af2a3d36b959081426dc
Mario Caballero Ramírez <mariocaballero@siani.es>
2023-05-11 09:46:41 +0100

    primitives: fix bug loading timetags when Week was added

--- libraries/primitives/src/io/intino/alexandria/Scale.java
+++ libraries/primitives/src/io/intino/alexandria/Scale.java
@@ -4,20 +4,26 @@ import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalUnit;
 
 public enum Scale {
-	Year(ChronoUnit.YEARS),
-	Month(ChronoUnit.MONTHS),
-	Week(ChronoUnit.WEEKS),
-	Day(ChronoUnit.DAYS),
-	Hour(ChronoUnit.HOURS),
-	Minute(ChronoUnit.MINUTES);
+	Year(ChronoUnit.YEARS, 4),
+	Month(ChronoUnit.MONTHS, 6),
+	Week(ChronoUnit.WEEKS, 6),
+	Day(ChronoUnit.DAYS, 8),
+	Hour(ChronoUnit.HOURS, 10),
+	Minute(ChronoUnit.MINUTES, 12);
 
 	private final TemporalUnit unit;
+	private final int digits;
 
-	Scale(TemporalUnit unit) {
+	Scale(TemporalUnit unit, int digits) {
 		this.unit = unit;
+		this.digits = digits;
 	}
 
 	public TemporalUnit temporalUnit() {
 		return unit;
+	}
+
+	public int digits() {
+		return digits;
 	}
 }
