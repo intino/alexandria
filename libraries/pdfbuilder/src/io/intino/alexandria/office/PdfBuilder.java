70ae1cd490004f623942a078f144bdf6190be965
cherrera <naitsirc98@gmail.com>
2023-06-14 12:40:26 +0100

    pdfbuilder: add docx4j backend implementation.

--- libraries/pdfbuilder/src/io/intino/alexandria/office/PdfBuilder.java
+++ libraries/pdfbuilder/src/io/intino/alexandria/office/PdfBuilder.java
@@ -3,27 +3,70 @@ package io.intino.alexandria.office;
 import fr.opensagres.poi.xwpf.converter.pdf.PdfConverter;
 import fr.opensagres.poi.xwpf.converter.pdf.PdfOptions;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.docx4j.Docx4J;
+import org.docx4j.openpackaging.exceptions.Docx4JException;
+import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
 
 import java.io.*;
+
+import static java.util.Objects.requireNonNull;
 
 public class PdfBuilder {
 
 	public static PdfBuilder create(File inputDocx) {
-		return new PdfBuilder(inputDocx);
+		return new PdfBuilder(inputDocx, new Docx4jBackend());
+	}
+
+	public static PdfBuilder create(File inputDocx, Backend backend) {
+		return new PdfBuilder(inputDocx, backend);
 	}
 
 	private final File inputDocx;
+	private final Backend backend;
 
-	public PdfBuilder(File inputDocx) {
+	public PdfBuilder(File inputDocx, Backend backend) {
 		this.inputDocx = inputDocx;
+		this.backend = requireNonNull(backend);
 	}
 
-	public void save(File file) throws IOException {
-		try(OutputStream out = new FileOutputStream(file)) {
-			try(InputStream doc = new FileInputStream(inputDocx)) {
-				XWPFDocument document = new XWPFDocument(doc);
-				PdfOptions options = PdfOptions.create();
-				PdfConverter.getInstance().convert(document, out, options);
+	public void save(File destinationPDF) throws IOException {
+		backend.save(inputDocx, destinationPDF);
+	}
+
+	public interface Backend {
+		void save(File inputDocx, File destinationPDF) throws IOException;
+	}
+
+	public static class ApachePOIBackend implements Backend {
+
+		@Override
+		public void save(File inputDocx, File destinationPDF) throws IOException {
+			try(OutputStream out = new BufferedOutputStream(new FileOutputStream(destinationPDF))) {
+				try(InputStream doc = new BufferedInputStream(new FileInputStream(inputDocx))) {
+					XWPFDocument document = new XWPFDocument(doc);
+					PdfOptions options = PdfOptions.create();
+					PdfConverter.getInstance().convert(document, out, options);
+				}
+			}
+		}
+	}
+
+	public static class Docx4jBackend implements Backend {
+
+		@Override
+		public void save(File inputDocx, File destinationPDF) throws IOException {
+			try(OutputStream outputStream = new BufferedOutputStream(new FileOutputStream(destinationPDF))) {
+				Docx4J.toPDF(readDocx(inputDocx), outputStream);
+			} catch (Docx4JException e) {
+				throw new IOException(e);
+			}
+		}
+
+		private WordprocessingMLPackage readDocx(File inputDocx) throws IOException {
+			try(InputStream inputStream = new FileInputStream(inputDocx)) {
+				return WordprocessingMLPackage.load(inputStream);
+			} catch (Docx4JException e) {
+				throw new IOException(e);
 			}
 		}
 	}
--- libraries/pdfbuilder/test/io/intino/alexandria/office/PdfBuilder_.java
+++ libraries/pdfbuilder/test/io/intino/alexandria/office/PdfBuilder_.java
@@ -1,11 +1,18 @@
 package io.intino.alexandria.office;
 
+import io.intino.alexandria.office.PdfBuilder.ApachePOIBackend;
+import io.intino.alexandria.office.PdfBuilder.Docx4jBackend;
+
 import java.io.File;
-import java.io.IOException;
 
 public class PdfBuilder_ {
 
-	public static void main(String[] args) throws IOException {
-		PdfBuilder.create(new File("temp/agenda.docx")).save(new File("temp/pdf222.pdf"));
+	public static void main(String[] args) throws Exception {
+		File inputDocx = new File("temp/20230614-agenda.docx");
+		File pdf1 = new File("temp/20230614-agenda-1.pdf");
+		File pdf2 = new File("temp/20230614-agenda-2.pdf");
+
+		PdfBuilder.create(inputDocx, new ApachePOIBackend()).save(pdf1);
+		PdfBuilder.create(inputDocx, new Docx4jBackend()).save(pdf2);
 	}
 }
