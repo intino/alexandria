dfcdf89e63502d2b619eb7164cb6285b0e6d5c54
Mario Caballero Ramírez <mariocaballero@siani.es>
2023-07-31 08:58:12 +0100

    ui: version deployed

--- framework/ui-elements/src/displays/components/RangeSlider.js
+++ framework/ui-elements/src/displays/components/RangeSlider.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { TextField } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 import { withSnackbar } from 'notistack';
 import AbstractRangeSlider from "../../../gen/displays/components/AbstractRangeSlider";
@@ -30,6 +31,43 @@ class RangeSlider extends AbstractRangeSlider {
 	getFormattedValue = () => {
 	    const selected = this.state.selected;
 		return selected != null && selected.value !== undefined ? selected.formattedValue + " " + this.translate("to") + " " + selected.formattedTo : 0 + " " + this.translate("to") + " " + 0;
+	};
+
+	renderValueEditor = () => {
+		const range = this.state.range;
+		return (
+		    <React.Fragment>
+                <TextField format={this.variant("body1")} type="number"
+                           value={this.state.editorValue} onChange={this.handleValueEditorChange.bind(this)} onKeyPress={this.handleValueEditorKeyPress.bind(this)}
+                           style={{width:'100%'}} autoFocus={true}
+                           inputProps={{
+                               min: range.min !== -1 ? range.min : undefined,
+                               max: range.max !== -1 ? range.max : undefined,
+                               step: 1
+                           }}/>
+                <TextField format={this.variant("body1")} type="number"
+                           value={this.state.editorTo} onChange={this.handleToEditorChange.bind(this)} onKeyPress={this.handleValueEditorKeyPress.bind(this)}
+                           style={{width:'100%',marginTop:'10px'}} autoFocus={true}
+                           inputProps={{
+                               min: range.min !== -1 ? range.min : undefined,
+                               max: range.max !== -1 ? range.max : undefined,
+                               step: 1
+                           }}/>
+            </React.Fragment>
+		);
+	};
+
+	showValueDialog = () => {
+	    this.setState({openValueDialog: true, editorValue: this.state.selected != null ? this.state.selected.value : this.state.range.min, editorTo: this.state.selected != null ? this.state.selected.to : this.state.range.min+1 });
+	};
+
+	handleToEditorChange = (e) => {
+	    this.setState({editorTo: e.target.value});
+	};
+
+	handleValueChange = () => {
+		this.requester.update({from: this.state.editorValue, to: this.state.editorTo});
+		this.setState({openValueDialog: false});
 	};
 
 	handleMoved = (e, newValue) => {
