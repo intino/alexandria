57e05129d5dd40df875d5485b2d15590626ecad9
cherrera <naitsirc98@gmail.com>
2023-08-10 13:24:53 +0100

    pdfbuilder: rollback poi version to 2.0.2 to fix pdf being bad rendered

--- libraries/pdfbuilder/test/io/intino/alexandria/office/PdfBuilder_.java
+++ libraries/pdfbuilder/test/io/intino/alexandria/office/PdfBuilder_.java
@@ -7,8 +7,13 @@ import java.io.File;
 public class PdfBuilder_ {
 
 	public static void main(String[] args) throws Exception {
-		File inputDocx = new File("temp/agenda.docx");
-		File pdf1 = new File("temp/agenda.pdf");
+		File inputDocx = new File("temp/PMBA.docx");
+		File pdf1 = new File("temp/PMBA.pdf");
+
+		PdfBuilder.create(inputDocx, new ApachePOIBackend()).save(pdf1);
+
+		inputDocx = new File("temp/agenda.docx");
+		pdf1 = new File("temp/agenda.pdf");
 
 		PdfBuilder.create(inputDocx, new ApachePOIBackend()).save(pdf1);
 	}
