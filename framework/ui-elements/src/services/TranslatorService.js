cdbc3c29f8dd17e8bd544164b4c65137bbb1fd23
Mario Caballero Ramírez <mariocaballero@siani.es>
2024-01-30 17:47:16 +0000

    ui, ui-framework: added family languages support

--- framework/ui-elements/src/services/TranslatorService.js
+++ framework/ui-elements/src/services/TranslatorService.js
@@ -1,12 +1,14 @@
 import I18n from "app-elements/gen/I18n";
 
 const TranslatorService = {
+    FamilyLanguages: { "mx": "es" },
+
     create : function (configuration) {
         let language = configuration.language;
         let translator = {};
 
         translator.translate = (word, params) => {
-            const i18n = I18n.load(language);
+            const i18n = translator.load(language);
             let wordTranslation = i18n != null && i18n[word] != null ? i18n[word] : word;
             return translator.translateParams(wordTranslation, params);
         };
@@ -15,6 +17,12 @@ const TranslatorService = {
             if (params == null) return text;
             for (var i=0; i<params.length; i++) text = text.replace("{" + (i+1) + "}", params[i]);
             return text;
+        };
+
+        translator.load = (language) => {
+            let i18n = I18n.load(language);
+            if (Object.keys(i18n).length === 0 && TranslatorService.FamilyLanguages[language] != null) i18n = I18n.load(TranslatorService.FamilyLanguages[language]);
+            return Object.keys(i18n).length !== 0 ? i18n : I18n.load("en");
         };
 
         return {
