47c8295a022644f1ed4413acdc234643c64da823
Mario Caballero Ramírez <mariocaballero@siani.es>
2026-04-21 08:46:16 +0100

    ui-elements, ui: added outined styles to SelectorRadioBox

--- framework/ui-elements/src/displays/components/SelectorRadioBox.js
+++ framework/ui-elements/src/displays/components/SelectorRadioBox.js
@@ -1,13 +1,15 @@
 import React from "react";
-import { withStyles } from '@material-ui/core/styles';
-import classNames from "classnames";
-import { Typography, Radio, FormControl, FormControlLabel, FormLabel, RadioGroup } from '@material-ui/core';
+import {withStyles} from '@material-ui/core/styles';
+import {FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, Typography} from '@material-ui/core';
 import AbstractSelectorRadioBox from "../../../gen/displays/components/AbstractSelectorRadioBox";
 import SelectorRadioBoxNotifier from "../../../gen/displays/notifiers/SelectorRadioBoxNotifier";
 import SelectorRadioBoxRequester from "../../../gen/displays/requesters/SelectorRadioBoxRequester";
 import DisplayFactory from "alexandria-ui-elements/src/displays/DisplayFactory";
 
 const styles = theme => ({
+	label: {
+		transform: "translate(0px, -2px) scale(0.8) !important"
+	}
 });
 
 class SelectorRadioBox extends AbstractSelectorRadioBox {
@@ -16,31 +18,32 @@ class SelectorRadioBox extends AbstractSelectorRadioBox {
 		super(props);
 		this.notifier = new SelectorRadioBoxNotifier(this);
 		this.requester = new SelectorRadioBoxRequester(this);
-        this.state = {
-            selected : "",
-            ...this.state
-        };
+		this.state = {
+			selected : "",
+			...this.state
+		};
 	};
 
 	render() {
-	    if (!this.state.visible) return (<React.Fragment/>);
+		if (!this.state.visible) return (<React.Fragment/>);
+		const { classes } = this.props;
 		const children = this.children();
 		const label = this.props.label;
 		const selected = this.state.selected != null ? this.state.selected : this.props.selected;
 		const layout = this.props.layout != null ? "layout " + this.props.layout.toLowerCase() + " wrap" : "layout vertical";
 		this._index = -1;
 		return (
-            <FormControl component="fieldset">
-                {label != null && label !== "" ? <FormLabel component="legend"><Typography variant={this.variant("subtitle1")} style={{color:this.props.color}}>{this.translate(label)}</Typography></FormLabel> : undefined }
-                <RadioGroup className={layout} value={selected} onChange={this.handleSelect.bind(this)}>
-                    {React.Children.map(children, (child, i) => { return this.renderItem(child); })}
-                </RadioGroup>
-            </FormControl>
+			<FormControl component="fieldset" variant="outlined" style={{ border: '1px solid rgba(0, 0, 0, 0.23)', borderRadius: '4px', padding: '10px' }}>
+				{label != null && label !== "" ? <FormLabel component="legend" className={classes.label}><Typography variant={this.variant("subtitle1")} style={{color:this.props.color}}>{this.translate(label)}</Typography></FormLabel> : undefined }
+				<RadioGroup className={layout} value={selected} onChange={this.handleSelect.bind(this)}>
+					{React.Children.map(children, (child, i) => { return this.renderItem(child); })}
+				</RadioGroup>
+			</FormControl>
 		);
 	};
 
 	renderItem = (item) => {
-        return (<FormControlLabel value={this._name(item)} control={<Radio style={{marginLeft:'10px',marginRight:'5px',padding:'0'}}>{item}</Radio>} label={this._label(item)} />);
+		return (<FormControlLabel value={this._name(item)} control={<Radio style={{marginLeft:'10px',marginRight:'5px',padding:'0'}}>{item}</Radio>} label={this._label(item)} />);
 	};
 
 	_name = (item) => {
