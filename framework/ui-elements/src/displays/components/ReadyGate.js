a9431b1cdcb1144bb69c9c238a0445a6c0e3e41f
Mario Caballero Ramírez <mariocaballero@siani.es>
2026-06-11 12:13:50 +0100

    konos, ui, ui-elements: added synchronization between frontend and backend to avoid blank pages

--- framework/ui-elements/src/displays/components/ReadyGate.js
+++ framework/ui-elements/src/displays/components/ReadyGate.js
@@ -1,8 +1,15 @@
 import React from "react";
+import {DisplayRouterContext} from "../DisplayRouter";
 
 export default class ReadyGate extends React.Component {
+	static contextType = DisplayRouterContext;
+
 	componentDidMount() {
 		const sendReady = () => {
+			if (this.context != null && this.context.ready != null) {
+				this.context.ready();
+				return;
+			}
 			if (Application != null && Application.services != null && Application.services.pushService != null) {
 				Application.services.pushService.ready();
 			}
